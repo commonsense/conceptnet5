50d972eb9499e2157a825d619665c13d9b63e8c4
Julian Chaidez <julianchaidez@Julian-Chaidezs-MacBook-Pro.local>
2011-09-21 18:01:21 -0400

    started concepts.py, with functions for creating and managing concepts

---
+++ conceptnet5/concepts.py
@@ -0,0 +1,29 @@
+"""
+Julian Chaidez, ConceptNet5
+_find_concept()
+_get_concept()
+"""
+
+def _create_concept(graph,language, name):
+
+    new_concept = graph.node(type = 'concept', language = language, name = name)
+    index_key = '/' + language + '/' + name
+    graph.nodes.indexes['concept_index']['index_key'] = new_concept
+    return new_concept
+
+def _find_concept(graph, language, name):
+
+    index_key = '/' + language + '/' + name
+    index = graph.nodes.indexes['concept_index']
+    result = index.query('name',index_key)[:])
+    if len(result): return result[0]
+    else: return None
+
+def _get_concept(graph, language, name):
+
+    concept = _find_concept(graph, language, name)
+    if not concept:
+        concept = _create_concept(graph, language, name)
+    return concept
+
+
