5e624687b8908a07fe3244ac3ac237bb0b3fd01d
Rob Speer <rspeer@mit.edu>
2011-09-21 18:45:01 -0400

    can get a key for an assertion

--- conceptnet5/assertion.py
+++ conceptnet5/assertion.py
@@ -1,6 +1,8 @@
+# Names cannot contain commas or colons.
+
 from neo4jrestclient.client import GraphDatabase, Index
 
-def get_arguments(assertion):
+def get_args(assertion):
     """
     Given an assertion, get its arguments as a list.
     
@@ -8,20 +10,31 @@ def get_arguments(assertion):
     called 'position' that will generally either be 1 or 2. (People find 1-indexing
     intuitive in this kind of situation.)
     """
-    edges = assertion.relationships.outgoing(types=['argument'])[:]
+    edges = assertion.relationships.outgoing(types=['arg'])[:]
     edges.sort(key = lambda edge: edge.properties['position'])
     if len(edges) > 0:
-        assert edges[0].properties['position'] == 1, "Arguments of %s are not 1-indexed"
+        assert edges[0]['position'] == 1, "Arguments of {0} are not 1-indexed".format(assertion)
     return [edge.end for edge in edges]
+
+def get_relation(assertion):
+    edges = assertion.relationships.outgoing(types=['relation'])
+    assert len(edges) == 1
+    return edges[0].end
 
 def assertion_key(node):
     """
     Takes in a node representing an Assertion, and constructs a unique key by
     which it can be identified.
     """
-    pass
+    rel = get_relation(node)['name']
+    arg_names = [arg['name'] for arg in get_args(node)]
+    arg_string = ','.join(arg_names)
+    lang = node['language']
+    return "/assertion/{0}/{1}:{2}".format(lang, rel, arg_string)
 
-g = GraphDatabase("http://new-caledonia.media.mit.edu:7474/db/data/")
-assertion = g.nodes[383]
-print get_arguments(assertion)
+if __name__ == '__main__':
+    g = GraphDatabase("http://new-caledonia.media.mit.edu:7474/db/data/")
+    assertion = g.nodes[387]
+    assertion['language'] = 'en'
+    print assertion_key(assertion)     # gives /assertion/en/Has:otter,fur
 
