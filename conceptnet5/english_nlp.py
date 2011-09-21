52af3c4e2bda90ee3dd88da0e3f23dcb9666d22f
Rob Speer <rspeer@mit.edu>
2011-09-21 16:15:41 -0400

    back to simplenlp for now

--- conceptnet5/english_nlp.py
+++ conceptnet5/english_nlp.py
@@ -1,18 +1,23 @@
-from nltk.corpus import wordnet
+#from nltk.corpus import wordnet
 import simplenlp
 EN = simplenlp.get('en')
 
 STOPWORDS = ['the', 'a', 'an']
 
 def morphy_stem(word):
+    from nltk.corpus import wordnet
+    morphy = wordnet.morphy
     word = word.lower()
-    return wordnet.morphy(word, 'v') or wordnet.morphy(word, 'n') or word
+    return morphy(word, 'v') or morphy(word, 'n') or word
+
+def simple_stem(word):
+    return EN.normalize(word)
 
 def tokenize(text):
     return EN.tokenize(text.strip()).split()
 
 def normalize(text):
-    pieces = [morphy_stem(word) for word in tokenize(text) if word not in STOPWORDS]
+    pieces = [simple_stem(word) for word in tokenize(text) if word not in STOPWORDS]
     return ' '.join(pieces)
 
 print normalize("this is a test")
