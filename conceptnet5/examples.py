7375a1579d25a1e98fdfa9e101b36c4272042762
Rob Speer <rspeer@mit.edu>
2011-09-21 17:56:28 -0400

    single example

---
+++ conceptnet5/examples.py
@@ -0,0 +1,6 @@
+"""
+Here are some examples of nodes and edges one could work with...
+"""
+from neo4jrestclient.client import GraphDatabase, Index
+otter = g.node(name='otter', language='en', type='concept')
+
