32af2c13a3ae666aa1e3c390cce890065a2c569d
Julian Chaidez <julianchaidez@Julian-Chaidezs-MacBook-Pro.local>
2011-09-21 18:48:31 -0400

    Added more functions

--- conceptnet5/concepts.py
+++ conceptnet5/concepts.py
@@ -8,8 +8,9 @@ _get_concept()
 def create_concept(graph,language, name):
 
     new_concept = graph.node(type = 'concept', language = language, name = name)
-    index_key = '/' + language + '/' + name
-    graph.nodes.indexes['concept_index']['index_key'] = new_concept
+    index_key = '/concept/' + language + '/' + name
+    if not 'concepts' in graph.nodes.indexes.keys(): graph.nodes.indexes.create('concepts')
+    graph.nodes.indexes['concepts']['index_key'] = new_concept
     return new_concept
 
 def find_concept(graph, language, name):
@@ -27,3 +28,15 @@ def get_concept(graph, language, name):
         concept = create_concept(graph, language, name)
     return concept
 
+def create_relation(graph, name):
+
+def find_relation(graph, name):
+
+def get_relation(graph, name):
+
+def get_id(node):
+
+    if node['type'] == 'relation': return '/rel/' + node['name']
+    elif node['type'] == 'concept': return '/' +  node['language'] + '/' + node['name']
+
+def 
