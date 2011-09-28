6f8a23d9e97b1b43f2694a570d5872babfacca73
Rob Speer <rspeer@mit.edu>
2011-09-28 16:59:55 -0400

    add ability to test URIs

--- conceptnet5/graph.py
+++ conceptnet5/graph.py
@@ -2,6 +2,16 @@
 from neo4jrestclient.client import GraphDatabase
 import urllib
 
+def uri_is_safe(uri):
+    """
+    Determines if this is a correctly-encoded URI, by checking for some
+    common problems that would cause it to be incorrect.
+
+    The output of encode_uri() should always pass uri_is_safe().
+    """
+    return (isinstance(uri, str) and ',' not in uri and ':' not in uri
+            and ' ' not in uri and '&' not in uri)
+
 def encode_uri(uri):
     """
     Takes in a URI and makes sure it follows our conventions:
@@ -24,7 +34,6 @@ def decode_uri(uri):
     return unquoted.replace('_', ' ')
 
 class ConceptNetGraph(object):
-
     def __init__(self, url):
         self.graph = GraphDatabase(url)
         self._index = self.graph.nodes.indexes['node_auto_index']
@@ -81,6 +90,9 @@ class ConceptNetGraph(object):
         return '/assertion/_' + relation_uri + '/_' + arg_uri_list.join('/_')
 
     def get_node(self, uri):
+        if not uri_is_safe(uri):
+            raise ValueError("This URI has unsafe characters in it. "
+                             "Please use encode_uri() first.")
         results = self._index.query('uri', uri)
         if len(results) == 1:
             return results[0]
@@ -90,21 +102,24 @@ class ConceptNetGraph(object):
             assert False, "Got multiple results for URI %r" % uri
 
     def get_or_create_node(self, uri):
-        uri = encode_uri(uri)
         return self.get_node(uri) or self._create_node(uri)
 
     def get_or_create_assertion(self, relation, args):
         uri = self._make_assertion_uri(self, relation['uri'],[arg['uri'] for arg in args])
         return self.get_node(uri) or self._create_assertion_w_components(self, relation, args)
 
+    def get_or_create_concept(self, language, name):
+        uri = "/concept/%s/%s" % (language, uri_encode(name))
+        
+
 if __name__ == '__main__':
     g = ConceptNetGraph('http://localhost:7474/db/data')
     a1 = g.get_or_create_node("/assertion/_/relation/IsA/_/concept/en/dog/_/concept/en/animal")
-    print a1['uri'], a1.id
 
     a2 = g.get_or_create_node(u"/assertion/_/relation/UsedFor/_/concept/zh_TW/枕頭/_/concept/zh_TW/睡覺")
     
     a3 = g.get_or_create_node("/assertion/_/relation/IsA/_/concept/en/test_:D/_/concept/en/it works")
+
     print a1['uri'], a1.id
     print a2['uri'], a2.id
     print a3['uri'], a3.id
