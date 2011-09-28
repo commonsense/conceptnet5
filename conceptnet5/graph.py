c554c7861cdf45da151152c46a4ec8b083d23728
Rob Speer <rspeer@mit.edu>
2011-09-28 15:04:31 -0400

    tried to do some URI encoding; now I have duplicates to clean up

--- conceptnet5/graph.py
+++ conceptnet5/graph.py
@@ -1,5 +1,27 @@
 # -*- coding: utf-8 -*-
 from neo4jrestclient.client import GraphDatabase
+import urllib
+
+def encode_uri(uri):
+    """
+    Takes in a URI and makes sure it follows our conventions:
+    
+    - expressed as a UTF-8 string
+    - spaces are changed to underscores
+    - URL-encoded, so for example a comma becomes %2C
+    """
+    if isinstance(uri, unicode):
+        uri = uri.replace(u' ', u'_').encode('utf-8', 'replace')
+    else:
+        uri = uri.replace(' ', '_')
+    return urllib.quote(uri)
+
+def decode_uri(uri):
+    """
+    Converts a URI to readable Unicode text.
+    """
+    unquoted = urllib.unquote(uri).decode('utf-8', 'replace')
+    return unquoted.replace('_', ' ')
 
 class ConceptNetGraph(object):
 
@@ -24,7 +46,7 @@ class ConceptNetGraph(object):
         return self.graph.node(
             type='concept',
             language=language,
-            name=name,
+            name=decode_uri(name),
             uri=uri
         )
 
@@ -32,7 +54,7 @@ class ConceptNetGraph(object):
         rel = rest
         return self.graph.node(
             type='relation',
-            name=rel,
+            name=decode_uri(rel),
             uri=uri
         )
     
@@ -68,6 +90,7 @@ class ConceptNetGraph(object):
             assert False, "Got multiple results for URI %r" % uri
 
     def get_or_create_node(self, uri):
+        uri = encode_uri(uri)
         return self.get_node(uri) or self._create_node(uri)
 
     def get_or_create_assertion(self, relation, args):
@@ -80,5 +103,9 @@ if __name__ == '__main__':
     print a1['uri'], a1.id
 
     a2 = g.get_or_create_node(u"/assertion/_/relation/UsedFor/_/concept/zh_TW/枕頭/_/concept/zh_TW/睡覺")
+    
+    a3 = g.get_or_create_node("/assertion/_/relation/IsA/_/concept/en/test_:D/_/concept/en/it works")
+    print a1['uri'], a1.id
     print a2['uri'], a2.id
+    print a3['uri'], a3.id
 
