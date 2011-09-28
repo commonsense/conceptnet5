e61d28cc1a98f4469967f1f7152e9ee262e7587b
Rob Speer <rspeer@mit.edu>
2011-09-28 14:21:22 -0400

    fix read_zh so filenames don't collide

--- conceptnet5/import_chinese/read_zh.py
+++ conceptnet5/import_chinese/read_zh.py
@@ -5,8 +5,8 @@ import codecs
 
 sparse_pieces = []
 for filename in os.listdir('.'):
-    if filename.startswith('conceptnet_zh'):
-        for line in codecs.open(filename, encoding='utf-8'):
+    if filename.startswith('conceptnet_zh_'):
+        for line in codecs.open(filename, encoding='utf-8', errors='replace'):
             line = line.strip()
             if line:
                 parts = line.split(', ')
@@ -19,4 +19,4 @@ for filename in os.listdir('.'):
                 sparse_pieces.append((1, concept2, left_feature))
 
 matrix = divisi2.make_sparse(sparse_pieces)
-divisi2.save(matrix, 'conceptnet_zh.smat')
+divisi2.save(matrix, 'feature_matrix_zh.smat')
