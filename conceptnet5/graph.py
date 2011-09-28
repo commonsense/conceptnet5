b7a505d0cc902e7fc90678e80d9777d4a6ecd0a7
Julian Chaidez <julianchaidez@Julian-Chaidezs-MacBook-Pro.local>
2011-09-28 16:52:54 -0400

    Annotation added to graph.py

--- conceptnet5/graph.py
+++ conceptnet5/graph.py
@@ -1,11 +1,30 @@
 
 class ConceptNetGraph(object):
 
-    def __init__(self, url)
+    def __init__(self, url):
+
+        """
+        initializes ConceptNetGraph,
+        creates GraphDatabase and node_index objects
+           
+        args:
+        url -- url of neo4j database in use
+        """
+
         self.graph = GraphDatabase(url)
         self._index = self.graph.nodes.indexes['node_auto_index']
 
-    def _create_node(self, uri):
+    def _create_node(self, uri, properties):
+
+        """
+        creates generic node object,
+        parses uri, takes out args, identifies type of node and runs relevant method
+        
+        args:
+        uri -- identifier of intended node, used in index
+        properties -- (optional) properties for assertions (see assertions)
+        """
+
         if uri.count('/') < 2:
             raise ValueError("""
             The URI %r is too short. You can't create the root or
@@ -15,9 +34,21 @@ class ConceptNetGraph(object):
         method = getattr(self, '_create_%s_node' % type)
         if method is None:
             raise ValueError("I don't know how to create type %r" % type)
-        return method(self, url, rest)
+        return method(self, url, rest, properties)
 
-    def _create_concept_node(self, uri, rest):
+    def _create_concept_node(self, uri, rest, properties):
+
+        """
+        creates concept node,
+        parses rest argument for language and name
+        returns node with concept parameters
+
+        args:
+        uri -- identifier of intended node, used in index
+        rest -- relevant parts of uri needed as parameters
+        properties -- (optional) properties for assertions (see assertions)
+        """
+
         language, name = rest.split('/')
         return self.graph.node(
             type='concept',
@@ -26,24 +57,63 @@ class ConceptNetGraph(object):
             uri=uri
         )
 
-    def _create_relation_node(self, uri, rest):
-        rel = rest
+    def _create_relation_node(self, uri, rest, properties):
+
+        """
+        creates relation node,                                  
+        uses rest as relation name
+        returns node with relation parameters
+
+        args: 
+        uri -- identifier of intended node, used in index
+        rest -- relevant parts of uri needed as parameters
+        properties -- (optional) properties for assertions (see assertions)        
+        """
+
+        name = rest
         return self.graph.node(
             type='relation',
             name=name,
             uri=uri
         )
     
-    def _create_assertion_node(self, uri, rest):
+    def _create_assertion_node(self, uri, rest, properties):
+
+        """
+        creates assertion node,
+        uses rest as to get relevant component uris and pull up the relevant nodes
+        assigns relationships
+        creates properties
+        returns assertion with parameters
+
+        args:
+        uri -- identifier of intended node, used in index
+        rest -- relevant parts of uri needed as parameters
+        properties -- properties for assertions
+        """
+
         rest = '/' + rest
         _,rel_uri,args_uris= rest.split('/_',2)
         arg_uris = arg_uris.split('/_')
         args = []
         rel = self.get_or_create_node(rel_uri)
         for arg_uri in arg_uris: args.append(self.get_or_create_node(arg_uri))
-        return self._create_assertion_w_components(uri, relation, args)
+        return self._create_assertion_w_components(uri, relation, args, properties)
 
-    def _create_assertion_w_components(self, uri, relation, args):
+    def _create_assertion_w_components(self, uri, relation, args, properties):
+
+        """
+        creates assertion node,
+        assigns relationships
+        creates properties
+        returns assertion with parameters
+
+        args:
+        uri -- identifier of intended node, used in index
+        rest -- relevant parts of uri needed as parameters
+        properties -- properties for assertions                 
+        """
+
         assertion = self.graph.node(   
             type='assertion', 
             uri=uri 
@@ -51,6 +121,8 @@ class ConceptNetGraph(object):
         assertion.relationships.create("relation", rel)
         for i in xrange(len(args)):
             assertion.relationships.create("arg", args[i], position=i+1)
+        for prop, value in properties.items():
+            assertion[prop] = value
         return assertion
 
     def _make_assertion_uri(self, relation_uri, arg_uri_list):
@@ -65,11 +137,11 @@ class ConceptNetGraph(object):
         else:
             assert False, "Got multiple results for URI %r" % uri
 
-    def get_or_create_node(self, uri):
-        return self.get_node(uri) or self._create_node(uri)
+    def get_or_create_node(self, uri, properties = {}):
+        return self.get_node(uri) or self._create_node(uri, properties)
 
-    def get_or_create_assertion(self, relation, args):
+    def get_or_create_assertion(self, relation, args, properties = {}):
         uri = self._make_assertion_uri(self, relation['uri'],[arg['uri'] for arg in args])
-        return self.get_node(uri) or self._create_assertion_w_components(self, relation, args)
+        return self.get_node(uri) or self._create_assertion_w_components(self, relation, args, properties)
 
 
