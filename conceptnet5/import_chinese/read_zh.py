408945b85f5d332df6c1afb4dabd823399576bbf
Rob Speer <rspeer@mit.edu>
2011-09-29 17:04:51 -0400

    worked on chinese conceptnet reader

--- conceptnet5/import_chinese/read_zh.py
+++ conceptnet5/import_chinese/read_zh.py
@@ -1,22 +1,27 @@
 from conceptnet.models import *
+from conceptnet5.graph import *
 import divisi2
 import os
 import codecs
 
-sparse_pieces = []
+def handle_file(filename):
+    for line in codecs.open(filename, encoding='utf-8', errors='replace'):
+        line = line.strip()
+        if line:
+            parts = line.split(', ')
+            user, frame_id, concept1, concept2 = parts
+            frame = Frame.objects.get(id=int(frame_id))
+            relation = frame.relation
+
+            assertion = u"/assertion/_/relation/%s/_/concept/zh_TW/%s/_/concept/zh_TW/%s" % (relation.name, concept1, concept2)
+            print encode_uri(assertion)
+
+            raw = u"/assertion/_/frame/%s/_/text/zh_TW/%s/_/text/zh_TW/%s" % (frame.text, concept1, concept2)
+            print encode_uri(raw)
+
+            # TODO: have raw justify assertion, have the 
+
 for filename in os.listdir('.'):
     if filename.startswith('conceptnet_zh_'):
-        for line in codecs.open(filename, encoding='utf-8', errors='replace'):
-            line = line.strip()
-            if line:
-                parts = line.split(', ')
-                user, frame_id, concept1, concept2 = parts
-                relation = Frame.objects.get(id=int(frame_id)).relation
-                left_feature = u"%s\\%s" % (concept1, relation)
-                right_feature = u"%s/%s" % (relation, concept2)
+        handle_file(filename)
 
-                sparse_pieces.append((1, concept1, right_feature))
-                sparse_pieces.append((1, concept2, left_feature))
-
-matrix = divisi2.make_sparse(sparse_pieces)
-divisi2.save(matrix, 'feature_matrix_zh.smat')
