351cf6d747c2629eb16c8c27f010e0767438930c
Julian Chaidez <julianchaidez>
2011-10-04 17:24:45 -0400

    finished annotation and cleaning up, changed several methods to global functions because of independence from class

--- conceptnet5/graph.py
+++ conceptnet5/graph.py
@@ -10,6 +10,10 @@ def list_to_uri_piece(lst):
     """
     Encode a list in a format suitable for a URI, by representing it in a
     form of JSON.
+
+    args:
+    lst -- the list which will be encoded
+
     """
     json_str = json.dumps(lst, ensure_ascii=False)
     if isinstance(json_str, unicode):
@@ -22,6 +26,9 @@ def uri_piece_to_list(uri):
     """
     Undo the effect of `list_to_uri_piece` by decoding the string from
     JSON.
+    
+    args:
+    uri -- the uri to be decoded into a list
     """
     return json.loads(uri)
 
@@ -33,6 +40,9 @@ def lucene_escape(text):
     prepending a backslash to them.
 
     This should only be done inside a neo4j index.query().
+
+    args:
+    text -- the text to be escaped
     """
     # The first two backslashes are understood by the expression as a
     # literal backslash. The final \1 refers to what the expression matched.
@@ -40,6 +50,30 @@ def lucene_escape(text):
     # Fun fact: if Python didn't have raw strings, the replacement string
     # would have to be '\\\\\\1'.
     return LUCENE_UNSAFE.sub(r'\\\1', text)
+
+def make_assertion_uri(relation_uri, arg_uri_list):
+    """
+    creates assertion uri out of component uris
+    
+    args:
+    relation_uri -- the uri of the relation being used i.e 'rel/IsA' or 'en/eat'
+    arg_uri_list -- the uris (in list form) of the arguments of the assertion
+    i.e ['/en/dog',...]
+
+    """
+    return '/assertion/' + list_to_uri_piece([relation_uri] + arg_uri_list)
+	    
+def make_list_uri(_type, args):
+    """
+    Creates any list-based uri out of component uris
+    
+    args:
+    _type -- the type of uri being made i.e assertion
+    args -- the argument uris i.e ['/en/eat','/en/dog/',..]
+
+    """
+    arglist = list_to_uri_piece(args)
+    return '/%s/%s' % (_type, arglist)
 
 def normalize_uri(uri):
     """
@@ -49,6 +83,9 @@ def normalize_uri(uri):
 
     We don't worry about URL-quoting here; the client framework takes
     care of that for us.
+
+    args:
+    uri -- the uri being normalized and returned
     """
     if isinstance(uri, str):
         uri = uri.decode('utf-8')
@@ -66,8 +103,12 @@ class ConceptNetGraph(object):
     """
     def __init__(self, domain):
         """
-        Create a ConceptNetGraph object, backed by a Neo4j database at the
-        given URL.
+        initializes ConceptGraph object,
+        connects with Neo4j database and calls indexes from that database
+
+        args:
+        domain -- url of the database that will be accessed and read by this graph object
+
         """
         #auth = get_auth()
         #url = 'http://%s:%s@%s/db/data' %\
@@ -86,6 +127,7 @@ class ConceptNetGraph(object):
         args:
         uri -- identifier of intended node, used in index
         properties -- (optional) properties for assertions (see assertions)
+
         """
         # Apply normalization to the URI here. All downstream functions can
         # assume it's normalized.
@@ -107,6 +149,13 @@ class ConceptNetGraph(object):
     def _create_edge(self, _type, source, target, properties = {}):
         """
 	Create an edge and ensure that it is indexed by its nodes.
+
+        args:
+        _type -- the type of edge, i.e 'justifies' or 'normalized'
+        source -- the source node of the edge
+        target -- the target node of the edge
+        properties -- (optional) properties to be attributed to this edge
+
 	"""
         source = self._any_to_node(source)
         target = self._any_to_node(target)
@@ -114,13 +163,19 @@ class ConceptNetGraph(object):
         edge['nodes'] = '%d-%d' % (source.id, target.id)
         return edge
 
-    def _create_assertion_from_components(self, uri, relation, args, properties):
+    def _create_assertion_w_components(self, uri, relation, args, properties):
         """
         A helper function used in creating assertions. Given that the
         relation and args have been found or created as nodes, use them to
 	create the assertion.
-        """
 
+        args:
+        uri -- the uri of the assertion being made
+        relation -- the relation being used in this assertion, in any form (uri, node, etc)
+        args -- the arguments being used in this assertion, in any form (uri, node, etc)
+        properties -- important properties of the assertion
+
+        """
         assertion = self.graph.node(
             type='assertion',
             uri=uri,
@@ -144,6 +199,7 @@ class ConceptNetGraph(object):
         uri -- identifier of intended node, used in index
         rest -- relevant parts of uri needed as parameters
         properties -- properties for assertions
+
         """
         uri_parts = uri_piece_to_list(rest)
         rel_uri = uri_parts[0]
@@ -152,7 +208,7 @@ class ConceptNetGraph(object):
         rel = self.get_or_create_node(rel_uri)
         for arg_uri in arg_uris:
             args.append(self.get_or_create_node(arg_uri))
-        assertion = self._create_assertion_from_components(uri, rel, args,
+        assertion = self._create_assertion_w_components(uri, rel, args,
                                                            properties)
 
         # Set a property to keep track of whether this assertion is normalized.
@@ -174,8 +230,8 @@ class ConceptNetGraph(object):
         uri -- identifier of intended node, used in index
         rest -- relevant parts of uri needed as parameters
         properties -- (optional) properties for assertions (see assertions)
-        """
 
+        """
         language, name = rest.split('/')
         return self.graph.node(
             type='concept',
@@ -196,8 +252,8 @@ class ConceptNetGraph(object):
         uri -- identifier of intended node, used in index
         rest -- relevant parts of uri needed as parameters
         properties -- properties for assertions (see _create_assertion_node function)
-        """
 
+        """
         language, name = rest.split('/')
         return self.graph.node(
             type='frame',
@@ -218,8 +274,8 @@ class ConceptNetGraph(object):
         uri -- identifier of intended node, used in index
         rest -- relevant parts of uri needed as parameters
         properties -- (optional) properties, mainly for assertions (see assertions)        
-        """
 
+        """
         name = rest
         return self.graph.node(
             type='relation',
@@ -238,8 +294,8 @@ class ConceptNetGraph(object):
         uri -- identifier of intended node, used in index
         rest -- relevant parts or uri needed in parameters
         properties -- (optional) properties for assertion (see assertions)
-        """
 
+        """
         name = rest.split('/')[-1]
         return self.graph.node(
             type='source',
@@ -248,21 +304,15 @@ class ConceptNetGraph(object):
             **properties
         )
 
-    def make_assertion_uri(self, relation_uri, arg_uri_list):
-        """creates assertion uri out of component uris"""
-        return '/assertion/' + list_to_uri_piece([relation_uri] + arg_uri_list)
-
-    def make_list_uri(self, _type, args):
-        """Creates any list-based uri out of component uris"""
-        arglist = list_to_uri_piece(args)
-        return '/%s/%s' % (_type, arglist)
-
     def get_node(self, uri):
         """
         searches for node in main index,
         returns either single Node, None or Error (for multiple results)
-        """
 
+        args:
+        uri -- the uri of the node in question
+
+        """
         uri = normalize_uri(uri)
         results = self._node_index.query('uri', lucene_escape(uri))
         if len(results) == 1:
@@ -279,6 +329,10 @@ class ConceptNetGraph(object):
 
         See this document for Lucene's syntax:
         http://lucene.apache.org/java/2_0_0/queryparsersyntax.html
+
+        args:
+        pattern -- the pattern that is being sought in the node uri
+
         """
         return self._node_index.query('uri', pattern)
 
@@ -286,6 +340,12 @@ class ConceptNetGraph(object):
         """
         Get an existing edge between two nodes with the specified type, or None
         if it doesn't exist.
+
+        args:
+        _type -- the type of edge being sought i.e justifies, normalized etc.
+        source -- the source of the edge being sought (the start)
+        target -- the target of the edge being sought (the end)
+
         """
         edges = self.get_edges(source, target)
         for edge in edges:
@@ -296,6 +356,11 @@ class ConceptNetGraph(object):
     def get_edges(self, source, target):
         """
         Get edges between `source` and `target`, specified as IDs or nodes.
+
+        args:
+        source -- the source of the edges in question (the start)
+        target -- the target of the edges in question (the end)
+
         """
         source = self._any_to_id(source)
         target = self._any_to_id(target)
@@ -305,6 +370,13 @@ class ConceptNetGraph(object):
         """
         Queries the edges from the node(s) given, from the start edge to the start edge,
         and returns them as a list (or list of lists if a list of nodes is provided
+
+        args:
+	nodes -- a node/uri/id or a list of any combination of those things
+	which is comprised of the source node(s) of the edges in question
+	start_edge -- the start edge, by order, which is being queried i.e edge 1
+	stop_edge -- the last edge, by order, which is being queried i.e edge 3
+
         """
         if type(nodes) == int:
             nodes = [nodes]
@@ -319,7 +391,13 @@ class ConceptNetGraph(object):
         return return_list
 
     def _any_to_id(self, obj):
-        """Converts any given input in the form of an id, uri or node into an id number."""
+        """
+        Converts any given input in the form of an id, uri or node into an id number.
+
+        args:
+        obj -- the object to be converted
+
+        """
         if isinstance(obj, Node):
             return obj.id
         elif isinstance(obj, basestring):
@@ -333,7 +411,13 @@ class ConceptNetGraph(object):
             raise TypeError
 
     def _any_to_node(self, obj, create=False):
-        """Converts any given input in the form of an id, uri or node into a node object."""
+        """
+        Converts any given input in the form of an id, uri or node into a node object.
+
+        args:
+        obj -- the object to be converted(/made)
+
+        """
         if isinstance(obj, Node):
             return obj
         elif isinstance(obj, basestring):
@@ -350,7 +434,13 @@ class ConceptNetGraph(object):
             raise TypeError
 
     def _any_to_uri(self, obj):
-        """Converts any given input in the form of an id, uri or node into a uri string."""
+        """
+        Converts any given input in the form of an id, uri or node into a uri string.
+
+        args:
+        obj -- the object to be converted
+
+        """
         if isinstance(obj, Node):
             return obj['uri']
         elif isinstance(obj, basestring):
@@ -363,8 +453,11 @@ class ConceptNetGraph(object):
     def get_node_by_id(self, _id):
         """
         Get a node by its ID in the database.
-        """
 
+        args:
+        _id -- the id of the node (numeric)
+
+        """
         return self.graph.nodes[_id]
 
     def get_or_create_node(self, uri, properties = {}):
@@ -374,16 +467,22 @@ class ConceptNetGraph(object):
         args:
         uri -- uri for node in question
         properties -- optional properties for assertion       
-        """
 
+        """
         return self.get_node(uri) or self._create_node(uri, properties)
 
     def get_or_create_edge(self, _type, source, target, properties = {}):
         """
         Get an edge of the specified `type` between `source` and `target`.
         If it doesn't exist, create it with the given properties.
-        """
 
+        args:
+        _type -- the type of edge i.e 'justifies', 'normalized' etc.
+        source -- the source node of the edge (the start)
+        target -- the target node of the edge (the end)
+        properties -- (optional) properties that can be attributed to the edge
+
+        """
         return (self.get_edge(_type, source, target) or
                 self._create_edge(_type, source, target, properties))
 
@@ -400,9 +499,11 @@ class ConceptNetGraph(object):
         properties -- properties for assertion
 
         """
-        uri = self.make_assertion_uri(self._any_to_uri(relation), [self._any_to_uri(arg) for arg in args])
+        uri = make_assertion_uri(self._any_to_uri(relation), \
+        [self._any_to_uri(arg) for arg in args])
         return (self.get_node(uri) or 
-        self._create_assertion_from_components(uri, self._any_to_node(relation, create=True),[self._any_to_node(arg, create=True) for arg in args], properties))
+        self._create_assertion_w_components(uri, self._any_to_node(relation, create=True), \
+        [self._any_to_node(arg, create=True) for arg in args], properties))
 
     def get_or_create_concept(self, language, name):
         """
@@ -412,6 +513,7 @@ class ConceptNetGraph(object):
         args:
         language -- language code ie. 'en'
         name -- name of concept ie. 'dog','fish' etc
+
         """
         uri = "/concept/%s/%s" % (language, name)
         return self.get_node(uri) or self._create_node(uri, {})
@@ -420,6 +522,10 @@ class ConceptNetGraph(object):
         """
         finds or creates a conjunction between nodes
         takes in conjunct arguments and returns conjuntion node
+
+        args:
+        conjuncts -- a list of the nodes to be connected to the conjunctions
+
         """
         conjuncts = [self._any_to_node(c) for c in conjuncts]
         uris = [c['uri'] for c in conjuncts]
@@ -487,6 +593,10 @@ class ConceptNetGraph(object):
         Arguments are represented in the graph as edges of type 'argument', with a property
         called 'position' that will generally either be 1 or 2. (People find 1-indexing
         intuitive in this kind of situation.)
+
+        args:
+        assertion -- the assertion (in any form, node, uri etc.) in question
+
         """
         assertion = self._any_to_node(assertion)
         edges = assertion.relationships.outgoing(types=['arg'])[:]
@@ -498,6 +608,12 @@ class ConceptNetGraph(object):
         Add an edge that justifies (or refutes) `target` using `source`.
         The weight represents the strength of the justification, from
         -1 to 1.
+
+        args:
+        source -- the source node of the intended justify edge in any form
+        target -- the target node of the justify edge in any form
+        weight -- a float 'weight' associated with the justification
+
         """
         edge = self.get_or_create_edge('justifies', source, target,
                                        {'weight': weight})
@@ -508,6 +624,10 @@ class ConceptNetGraph(object):
         """
         Given a node object, recompute the 'justification' score based on the justify nodes.
 	Can be run after the justify nodes are updated with new nodes or new value.
+
+        args:
+        node -- the node whose score is being recomputed
+
         """
         score = 0
         for edge in node.relationships.incoming(types=['justify']):
@@ -522,6 +642,11 @@ class ConceptNetGraph(object):
 
         Also adds a justification edge, which should have a positive
         weight.
+
+        args:
+        source -- the source node, the 'justifier'
+	target -- the targate node, the node being justified
+        weight -- the weight of the normalized edge
         """
 
         assert weight > 0
@@ -561,8 +686,14 @@ class ConceptNetGraph(object):
             for conjunction in conj_list:
                 conjunction.delete()
             node.delete()
-        else: assert False, "There are other nodes that are dependent on this node"
+        else: assert False, \
+        "There are other nodes that are dependent on this node"
 
 def get_graph():
-    """Return a graph object representing the Concept Net graph hosted on the tortoise server for the Concept Net team."""
+    """
+    Return a graph object representing the Concept Net graph hosted
+    on the tortoise server for the Concept Net team.
+
+    no args
+    """
     return ConceptNetGraph('tortoise.csc.media.mit.edu')
