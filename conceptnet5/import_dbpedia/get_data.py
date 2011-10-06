02037a11c851540d8b241020f2024c57e0f71df2
Justin Venezuela <jven@mit.edu>
2011-10-06 14:26:02 -0400

    Catch Unicode decoding exception when decoding Wikipedia titles.

--- conceptnet5/import_dbpedia/get_data.py
+++ conceptnet5/import_dbpedia/get_data.py
@@ -9,9 +9,12 @@ import urllib2
 
 DBPEDIA_DATA_PREFIX = u'http://dbpedia.org/page/'
 DBPEDIA_SOURCE = [u'source', u'web', u'dbpedia.org']
+TYPE_HTML = ('<a class="uri" href="http://www.w3.org/1999/02/'
+      '22-rdf-syntax-ns#type">')
 TYPE_RELATION = u'rdf:type'
 TYPE_RELATION_PROP_KEY = u'owl:sameAs'
 TYPE_RELATION_PROP_VAL = u'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
+WIKIPEDIA_TITLES = 'wikipedia.txt'
 
 VERBOSE = True
 
@@ -23,27 +26,27 @@ def show_message(message):
     print message
 
 def get_url_from_obj_name(obj_name):
-  show_message('NOTICE: Getting data for object \'%s\'.' % obj_name)
   obj_url = DBPEDIA_DATA_PREFIX + obj_name
   return obj_url
 
 def get_html_from_url(url):
-  show_message('NOTICE: Opening DBPedia page...')
   page = urllib2.urlopen(url)
   html = page.read()
   page.close()
-  show_message('NOTICE: Done.')
   return html
 
 def get_types_from_html(html):
-  show_message('NOTICE: Parsing...')
+  show_message(u'NOTICE: Parsing...')
+  if TYPE_HTML not in html:
+    show_message(u'WARNING: No types found.')
+    return []
   obj_types = []
-  html = html.split('<a class="uri" href="http://www.w3.org/1999/02/'
-      '22-rdf-syntax-ns#type">', 1)[1].split('<ul>', 1)[1].split('</ul>', 1)[0]
+  show_message(u'NOTICE: ---TYPES FOUND! :D Extracting...---')
+  html = html.split(TYPE_HTML, 1)[1].split('<ul>', 1)[1].split('</ul>', 1)[0]
   while 'href="' in html:
     [obj_type, html] = html.split('href="', 1)[1].split('">', 1)
     obj_types.append(obj_type)
-  show_message('NOTICE: Done.')
+  show_message(u'NOTICE: Done.')
   return obj_types
 
 def make_type_assertions_for_obj(conceptnet, obj_url, obj_types):
@@ -56,17 +59,26 @@ def make_type_assertions_for_obj(conceptnet, obj_url, obj_types):
     assertion = conceptnet.get_or_create_assertion(
         relation, [concept, obj_type_concept])
     conceptnet.justify(source, assertion)
-  print 'concept: %s' % concept
-  print 'relation: %s' % relation
-  print 'source: %s' % source
 
 def main():
   conceptnet = get_graph()
-  obj_names = ['Tetris']
-  for obj_name in obj_names:
+  #wikipediaTitles = open(WIKIPEDIA_TITLES)
+  wikipediaTitles = ['"!!Tetris"', '"Tetris"', 'Tetris']
+  for line in wikipediaTitles:
+    try:
+      obj_name = line.strip().decode('utf-8')
+    except:
+      show_message(u'WARNING: Could not decode \'%s\'.' % line)
+      continue
     # get data from dbpedia
     obj_url = get_url_from_obj_name(obj_name)
-    html = get_html_from_url(obj_url)
+    try:
+      html = get_html_from_url(obj_url)
+      show_message(u'NOTICE: Found DBPedia page for \'%s\'.' % obj_name)
+    except:
+      show_message(
+          u'WARNING: Could not get DBPedia page for \'%s\'.' % obj_name)
+      continue
     obj_types = get_types_from_html(html)
     # interact with graph
     make_type_assertions_for_obj(conceptnet, obj_url, obj_types)
