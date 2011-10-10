fa4fc78d5584c89413234b23f4b17bf37f703de1
Justin Venezuela <jven@mit.edu>
2011-10-10 14:10:09 -0400

    Adding static content and HTML templates.

--- conceptnet5/web_interface/main.py
+++ conceptnet5/web_interface/main.py
@@ -5,6 +5,8 @@ Web interface for ConceptNet5.
 __author__ = 'Justin Venezuela (jven@mit.edu)'
 
 from flask import Flask
+from flask import render_template
+from flask import url_for
 from conceptnet5.graph import get_graph
 
 app = Flask(__name__)
@@ -12,7 +14,7 @@ conceptnet = get_graph()
 
 @app.route('/')
 def main():
-  return 'Hello world!'
+  return render_template('home.html')
 
 @app.route('/web/<path:uri>')
 def get_data(uri):
