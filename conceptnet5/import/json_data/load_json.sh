69c42f53c671c5f2bc6a4b05870d6f7442e1e726
Rob Speer <rspeer@mit.edu>
2011-10-17 17:56:37 -0400

    update server IP

--- conceptnet5/import/json_data/load_json.sh
+++ conceptnet5/import/json_data/load_json.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export MONGO_HOST=67.202.46.13
+export MONGO_HOST=67.202.5.17
 mongoimport --upsert --upsertFields uri -h $MONGO_HOST -d conceptnet -c nodes $1.nodes.json
 mongoimport --upsert --upsertFields key -h $MONGO_HOST -d conceptnet -c edges $1.edges.json
 mongoimport -h $MONGO_HOST -d conceptnet -c scoredEdges $1.scored.json
