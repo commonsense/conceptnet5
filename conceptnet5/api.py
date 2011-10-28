0a968a3c0dbc0f0f4151fda816725395708b40fa
Julian Chaidez <julianchaidez>
2011-10-28 03:57:08 -0400

    altered system of inputing arguments to be expandable and added framework for additional functions. Also setup system for dealing with invalid parameters.

--- conceptnet5/api.py
+++ conceptnet5/api.py
@@ -24,6 +24,20 @@ else:
     root_url = sys.argv[1]
 
 no_leading_slash = ['http']
+"""
+request keys is a dictionary which stores a record of which request paramaters
+are permitted, what their default value is, what type they should return,
+what their limiting values are, and whether or not they should be ignored
+in the case of a request in which they could serve as the parameter for multiple
+requests in a batch.
+
+The dictionaries for request parameters contain the following:
+'request_name':{'default':__,'type':__,'upper':__,'lower':__ OR 'valid':[__],
+'ignore':__]
+"""
+request_keys = {'get':{'default':None,'type':str,'valid':None,'ignore':False},\
+        'rpp':{'default':50,'type':int,'upper':100,'lower':1,'ignore':False},
+        'max_score':{'default':0.0,'type':float,'upper':None,'lower':0,'ignore':True}}
 
 def correct_uri(uri):
     """
@@ -46,6 +60,54 @@ def add_slash(uri):
     else:
         return '/' + uri
 
+def create_arg_dict(ignore_specific):
+    """
+    This function creates the argument dictionary out of the request dictionary
+    created by flask. It ignores query specific parameters in the case of a general
+    request for more than a single type of edge or node, and it allows the 
+    creation of such a dictionary to be moved out of the more local request functions
+    """
+
+    args = flask.request.args
+    return_args = {}
+    if ignore_specific:
+        for k in request_keys.keys():
+            if k in args and k in request_keys:
+                if request_keys[k]['ignore']: return_args[k] = request_keys[k]['default']
+                else: 
+                    return_args[k],error_message = check_valid(args.get(k,type=request_keys[k]['type']),k,request_keys[k])
+                    if error_message != '':
+                        return error_message
+            else:
+                return_args[k] = request_keys[k]['default']
+    else:
+        for k in request_keys.keys():
+            if k in args:
+                return_args[k],error_message = check_valid(args.get(k,type=request_keys[k]['type']),k,request_keys[k])
+                if error_message != '':
+                    return error_message
+            else: return_args[k] = request_keys[k]['default']
+    return return_args
+
+def check_valid(arg, name, request_params):
+    """
+    This function confirms the validity or invalidity of a parameter as specified
+    by the request_keys dict.
+    """
+    if type(arg) == int or type(arg) == float:
+        if arg < request_params['lower'] or (arg > request_params['upper'] and request_params['upper'] != None):
+            return None, 'the parameter ' + name + ' was given an invalid value.\
+            Please only request values between ' + str(request_params['lower']) + ' and '\
+            + str(request_params['upper']) + '.'
+        else:
+            return arg, ''
+    elif type(arg) == str:
+        if not request_params['valid'] or arg in request_params['valid']:
+            return arg, ''
+        else:
+            return None, 'the parameter ' + name + ' was given the invalid value: ' + arg\
+            + '. Please only request one of the following values: ' + ', '.join(request_params['valid'])
+
 @app.route('/<path:uri>/')
 def get_data(uri):
     """
@@ -53,21 +115,26 @@ def get_data(uri):
     json data.
     """
     json = {}
-    print correct_uri(uri)
     if concept_graph.get_node(correct_uri(uri)) != None:
-        request_list = flask.request.args.get('get')
-        if request_list == None:
+        requests = flask.request.args.get('get')
+        if requests == None or len(requests.split(' ')) != 1:
+            args = create_arg_dict(True)
+        else:
+            args = create_arg_dict(False)
+        if type(args) == str:
+            return flask.Response(response=flask.json.dumps({'error':args}),status=404,mimetype='json')
+        if requests == None:
             for k,f_list in valid_requests.iteritems():
                 for label,f in f_list.iteritems():
-                    json[label] = f(uri)
+                    json[label] = f(uri,args)
             return flask.jsonify(json)
         else:
-            request_list = request_list.split(' ')
+            requests = requests.split(' ')
             valid = True
-            for i in request_list:
+            for i in requests:
                 if i in valid_requests.keys():
                     for label,f in valid_requests[i].iteritems():
-                        json[label] = f(uri)
+                        json[label] = f(uri,args)
                 else:
                     valid = False
                     break
@@ -79,7 +146,7 @@ def get_data(uri):
     else:
         return flask.Response(response=flask.json.dumps({'error':'invalid uri ' + correct_uri(uri)}),status=404,mimetype='json')
 
-def get_properties(uri):
+def get_properties(uri,args):
     """
     This function retrieves information about the node in question.
     """
@@ -88,89 +155,84 @@ def get_properties(uri):
     del node['_id']
     return node
 
-def get_incoming_assertions(uri):
+def get_incoming_assertions(uri,args):
     """
     This function retrieves information about which assertions point to
     the node in question. It does so in a paginated format based on max score.
     """
     json = []
-    max_score = flask.request.args.get('max_score',type=float)
-    if max_score == None:
-        max_score = 0.0
-    new_max_score = 0
-    for relation in concept_graph.get_incoming_edges(correct_uri(uri), _type='arg', max_score=max_score, result_limit=50):
+    new_max_score = 0.0
+    for relation in concept_graph.get_incoming_edges(correct_uri(uri), _type='arg', max_score=args['max_score'], result_limit=args['rpp']):
         json.append(concept_graph.get_node(correct_uri(relation[1])))
         json[-1]['url'] = root_url  + add_slash(json[-1]['uri'])
         del json[-1]['_id']
         new_max_score = relation[0]['score']
-    if len(json) == 50:
+    if len(json) == args['rpp']:
         json.append({})
-        json[-1]['next'] = root_url + add_slash(uri) + '?get=incoming_assertions&max_score='+str(new_max_score)
+        json[-1]['next'] = root_url + add_slash(uri)\
+        + '?get=incoming_assertions'+ '&rpp=' + str(args['rpp']) + '&max_score=' + str(new_max_score)
     return json
 
-def get_incoming_assertions_url(uri):
+def get_incoming_assertions_url(uri,args):
     """
     This function returns a url linking to a json page with only information
     on incoming assertions
     """
     return root_url + add_slash(uri) + '?get=incoming_assertions'
 
-def get_incoming_edges(uri):
+def get_incoming_edges(uri,args):
     """
     This function retrieves information about the incomign edges of
     the node in question. It does so in a paginated format based on max score.
     """
     json = []
-    max_score = flask.request.args.get('max_score',type=float)
-    if max_score == None:
-        max_score = 0.0
-    for relation in concept_graph.get_incoming_edges(correct_uri(uri), max_score=max_score, result_limit=50):
+    for relation in concept_graph.get_incoming_edges(correct_uri(uri), max_score=args['max_score'], result_limit=args['rpp']):
         json.append(relation[0])
         try: del json[-1]['_id']
         except KeyError: pass
         del json[-1]['jitter']
-    if len(json) == 50:
+    if len(json) == args['rpp']:
         json.append({})
-        json[-1]['next'] = root_url + add_slash(uri) + '?get=incoming_edges&max_score=' + str(json[-2]['score'])
+        json[-1]['next'] = root_url + add_slash(uri)\
+        + '?get=incoming_edges'+ '&rpp=' + str(args['rpp']) + '&max_score=' + str(json[-2]['score'])
     return json
 
-def get_incoming_edges_url(uri):
+def get_incoming_edges_url(uri,args):
     """
     This function returns a url linking to a json page with only information
     on incoming edges
     """
     return root_url + add_slash(uri) + '?get=incoming_edges'
 
-def get_outgoing_edges(uri):
+def get_outgoing_edges(uri,args):
     """
     This function retrieves information about the outgoing edges of
     the node in question. It does so in a paginated format based on max score.
     """
     json = []
-    max_score = flask.request.args.get('max_score',type=float)
-    if max_score == None:
-        max_score = 0.0
-    for relation in concept_graph.get_outgoing_edges(correct_uri(uri), max_score=max_score, result_limit=50):
+    for relation in concept_graph.get_outgoing_edges(correct_uri(uri), max_score=args['max_score'], result_limit=args['rpp']):
         json.append(relation[0])
         try: del json[-1]['_id']
         except KeyError: pass
         del json[-1]['jitter']
-    if len(json) == 50:
+    if len(json) == args['rpp']:
         json.append({})
-        json[-1]['next'] = root_url + add_slash(uri) + '?get=outgoing_edges&max_score=' + str(json[-2]['score'])
+        json[-1]['next'] = root_url + add_slash(uri)\
+        + '?get=outgoing_edges'+ '&rpp=' + args['rpp'] + '&max_score=' + str(json[-2]['score'])
     return json
 
-def get_outgoing_edges_url(uri):
+def get_outgoing_edges_url(uri,args):
     """
     This function returns a url linking to a json page with only information
     on outgoing edges.
     """
     return root_url + add_slash(uri) + '?get=outgoing_edges'
 
-def get_word_senses(uri):
+def get_word_senses(uri,args):
     """
     This function retrieves information about the word senses related to
     the node in question. It gives all wordsenses in one go.
+    It also yields all word senses of those word senses by default.
     """
     json = []
     for relation in concept_graph.get_incoming_edges(correct_uri(uri), _type='senseOf'):
@@ -179,10 +241,10 @@ def get_word_senses(uri):
         del json[-1]['_id']
     return json
 
-def get_word_sense_of(uri):
+def get_word_sense_of(uri,args):
     """
     This function retrieves information about the nodes that this node
-    is a word sense of. It ives all wordsenses in one go.
+    is a word sense of. It yields all wordsenses in one go.
     """
     json = []
     for relation in concept_graph.get_outgoing_edges(correct_uri(uri), _type='senseOf'):
@@ -191,7 +253,7 @@ def get_word_sense_of(uri):
         del json[-1]['_id']
     return json
 
-def get_normalized(uri):
+def get_normalized(uri,args):
     """
     This function retrieves information about the nodes that are normalized
     concepts of this node, if there are any.
@@ -203,7 +265,7 @@ def get_normalized(uri):
         del json[-1]['_id']
     return json
 
-def get_normalized_of(uri):
+def get_normalized_of(uri,args):
     """
     This function retrieves information about the nodes that this node is a
     normalized version of, if there are any.
@@ -233,4 +295,4 @@ valid_requests = {'incoming_edges':{'incoming_edges':get_incoming_edges,\
              'normalized_of':{'normalized_of':get_normalized_of}}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
