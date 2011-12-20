9d8735e888d74b97236b23542178e9e34f895a75
Rob Speer <rspeer@mit.edu>
2011-12-20 14:50:11 -0500

    send json to the shard server

--- import/json_data/load_json.sh
+++ import/json_data/load_json.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-source secrets.sh
-export MONGO_HOST=67.202.1.34
-mongoimport --upsert --upsertFields uri -h $MONGO_HOST -u $USERNAME -p $PASSWORD -d conceptnet -c nodes $1.nodes.json
-mongoimport --upsert --upsertFields key -h $MONGO_HOST -u $USERNAME -p $PASSWORD -d conceptnet -c edges $1.edges.json
-#mongoimport -h $MONGO_HOST -d conceptnet -c scoredEdges $1.scored.json
+#source secrets.sh
+export MONGO_HOST=ganymede.csc.media.mit.edu:30000
+mongoimport --upsert --upsertFields uri -h $MONGO_HOST -d conceptnet-small -c nodes $1.nodes.json
+mongoimport --upsert --upsertFields key -h $MONGO_HOST -d conceptnet-small -c edges $1.edges.json
