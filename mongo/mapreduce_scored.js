4e5ddc6d59735667b4a612dc18be16378bd44b72
Rob Speer <rspeer@mit.edu>
2011-12-22 05:07:17 -0500

    well this doesn't actually work, but might as well keep it for now

--- mongo/mapreduce_scored.js
+++ mongo/mapreduce_scored.js
@@ -1,15 +1,7 @@
-mapNodeScores = function () {
-    // take in a justification entry
-    var node = db.nodes.findOne({'uri': this._id});
-    // add the score, plus some jitter for uniqueness
-    node.jitter = Math.random() * .000001;
-    node.score = this.value + node.jitter;
-    emit(this._id, node);
-};
-
 mapEdgeScores = function () {
-    var scored1 = db.justification.findOne({'_id': this.start});
-    var scored2 = db.justification.findOne({'_id': this.end});
+    var scored1 = db.nodeScores.findOne({'_id': this.start});
+    var scored2 = db.nodeScores.findOne({'_id': this.end});
+
     this.jitter = Math.random() * 0.001;
     var score1 = this.jitter;
     var score2 = this.jitter;
@@ -18,6 +10,7 @@ mapEdgeScores = function () {
 
     var scoreCodeIn = toScoreCode(score1);
     var scoreCodeOut = toScoreCode(score2);
+    
     var codeAout = 'Aout ' + this.start + ' ' + scoreCodeOut + ' ' + this.end;
     var codeAin = 'Ain ' + this.end + ' ' + scoreCodeIn + ' ' + this.start;
     var codeBout = 'Bout ' + this.type + ' ' + this.start + ' ' + scoreCodeOut + ' ' + this.end;
@@ -33,10 +26,5 @@ reduceNop = function (key, values) {
     return values[0];
 };
 
-//db.justification.mapReduce(mapNodeScores, reduceNop, {out: 'scoredNodes'});
-db.edges.mapReduce(mapEdgeScores, reduceNop, {out: 'scoredEdges'});
-db.scoredEdges.ensureIndex({'value.start': 1, 'value.type': 1, 'value.score': 1})
-db.scoredEdges.ensureIndex({'value.end': 1, 'value.type': 1, 'value.score': 1})
-db.scoredEdges.ensureIndex({'value.start': 1, 'value.score': 1})
-db.scoredEdges.ensureIndex({'value.end': 1, 'value.score': 1})
+db.edges.mapReduce(mapEdgeScores, reduceNop, {out: 'edgeScores'});
 
