850fe5ba07beb8c0edd258322825dd338cba492b
jvarley <jvarley@amber.media.mit.edu>
2013-03-01 18:53:46 -0500

    fixed flat_assertions, now uses temporary files so it does not hold all of the assertions in memory at any point

--- cnet5_index_builder/cnet5-data/run.sh
+++ cnet5_index_builder/cnet5-data/run.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-nohup make all -j 4 > run_output.txt &
+
+run_func(){
+ if [ -f run_output.txt ]; then rm run_output.txt; fi
+echo "started at: $(date)"
+make all -j 4
+echo "finished at $(date)"
+ return $TRUE
+}
+
+nohup $(run_func) &
--- cnet5_index_builder/cnet5-data/scripts/flat_assertions.py
+++ cnet5_index_builder/cnet5-data/scripts/flat_assertions.py
@@ -1,31 +1,54 @@
 import codecs
 from conceptnet5.edges import make_edge, MultiWriter
 from collections import defaultdict
+from multiprocessing import Process
+import time
+import os
+import sys
 
-
+N = 100
+CURRENT_DIR = os.getcwd()
 
 def build_core_from_csvs(csv_files):
 
+    for csv_file in csv_files:
+        sort_assertions(csv_file)
+
+    for assertions_file_index in range(N):
+        sum_assertions(assertions_file_index)
+        
+
+#this hashes all the uri's and places them
+#into smaller grouping of uri's that have the
+#same hash, that way we can them sum up all the weights for
+#each one of these files without having to pull all 
+#assertions into memory at the same time.
+def sort_assertions(csv_file):
+    for line in codecs.open(csv_file, encoding='utf-8'):
+        uri, rel, start, end, context, weight, sources, id, dataset = line.split('\t')[:9]
+        uri_index = hash(uri)%N
+        f = codecs.open(CURRENT_DIR +'/data/temp/core_'+str(uri_index)+'.txt', 'a','utf-8')
+        f.write(line)
+        f.close()
+
+
+def sum_assertions(file_index):
     weights = defaultdict(float)
     assertions = {}
     ccby = defaultdict(bool)
 
+    for line in codecs.open(CURRENT_DIR +'/data/temp/core_'+str(file_index)+'.txt', 'r','utf-8'):
+        uri, rel, start, end, context, weight, sources, id, dataset = line.split('\t')[:9]
+        if uri != 'uri' and context == '/ctx/all':
+            weight = float(weight)
+            weights[uri] += float(weight)
+            assertions[uri] = (rel, start, end, context, weights[uri])
+            if not (dataset.startswith('/d/reverb') or dataset.startswith('/d/wiktionary') or dataset.startswith('/d/dbpedia')):
+                ccby[uri] = True
 
-    for csv_file in csv_files:
-        print "currently in file: " + str(csv_file)
-        for line in codecs.open(csv_file, encoding='utf-8'):
-            uri, rel, start, end, context, weight, sources, id, dataset = line.split('\t')[:9]
-            if uri != 'uri' and context == '/ctx/all':
-                weight = float(weight)
-                weights[uri] += float(weight)
-                assertions[uri] = (rel, start, end, context, weights[uri])
-                if not (dataset.startswith('/d/reverb') or dataset.startswith('/d/wiktionary') or dataset.startswith('/d/dbpedia')):
-                    ccby[uri] = True
 
-    print 'writing'
     writer_core = MultiWriter('assertion_totals_core')
     #writer_sa = MultiWriter('assertion_totals_sa')
-
     for uri, values in assertions.iteritems():
         relation, start, end, context, weight = values
         if ccby[uri]:
@@ -43,10 +66,9 @@ def build_core_from_csvs(csv_files):
     #writer_sa.close()
 
 
-
 if __name__ == '__main__':
-    import sys
     csv_files = sys.argv[1:]
     print "building core from the following: \n" + str(csv_files)
     build_core_from_csvs(csv_files)
+    print "finished building the core"
 
