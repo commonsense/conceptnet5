906daa7c1749e08330bc787dccabbe8fa8614745
jvarley <jakevarley@gmail.com>
2013-03-28 11:28:38 -0400

    created make_runner.sh, which can run all the make commands and will not terminiate when ssh conncetion is cut.  Also got rid of unused bash scripts. Added solr scripts to the make file rather than having them as standalone scripts.  Now everything is run through the make file

--- cnet5_index_builder/cnet5-data/clear_solr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# http://wiki.apache.org/solr/FAQ#How_can_I_delete_all_documents_from_my_index.3F
-# http://wiki.apache.org/solr/UpdateXmlMessages#Updating_a_Data_Record_via_curl
- 
- #curl "http://burgundy.media.mit.edu:8983/solr/select?q=*:*"
-
- curl "http://salmon.media.mit.edu:8983/solr/update?commit=true" -H "Content-Type: text/xml" --data-binary  '<delete><query>*:*</query></delete>'
