4efde824ef891e854c0ba70269a398f9fdcf71d7
Rob Speer <rspeer@mit.edu>
2014-02-21 14:26:37 -0500

    Update and document language codes.
    Remove Minangkabau, a language I don't think we intend to show in the
    web interface. Perhaps the code was intended to be a different language.

--- conceptnet5/language_codes.py
+++ conceptnet5/language_codes.py
@@ -31,14 +31,14 @@ SUPPORTED_LANGUAGE_CODES = [
     'hr', 'ht', 'hu', 'hy', 'hz', 'ia', 'id', 'ie', 'ig', 'ii', 'ik', 'io',
     'is', 'it', 'iu', 'ja', 'jv', 'ka', 'kg', 'ki', 'kj', 'kk', 'kl', 'km',
     'kn', 'ko', 'kr', 'ks', 'ku', 'kv', 'kw', 'ky', 'la', 'lb', 'lg', 'li',
-    'ln', 'lo', 'lt', 'lu', 'lv', 'mg', 'mh', 'mi', 'min', 'mk', 'ml', 'mn',
-    'mr', 'ms', 'mt', 'my', 'na', 'nb', 'nd', 'ne', 'ng', 'nl', 'nn', 'no',
-    'nr', 'nv', 'ny', 'oc', 'oj', 'om', 'or', 'os', 'pa', 'pi', 'pl', 'ps',
-    'pt', 'qu', 'rm', 'rn', 'ro', 'ru', 'rw', 'sa', 'sc', 'sco', 'sd', 'se',
-    'sg', 'si', 'sk', 'sl', 'sm', 'sn', 'so', 'sq', 'sr', 'ss', 'st', 'su',
-    'sv', 'sw', 'ta', 'te', 'tg', 'th', 'ti', 'tk', 'tl', 'tn', 'to', 'tr',
-    'ts', 'tt', 'tw', 'ty', 'ug', 'uk', 'ur', 'uz', 've', 'vi', 'vo', 'wa',
-    'wo', 'xh', 'yi', 'yo', 'za', 'zh', 'zu'
+    'ln', 'lo', 'lt', 'lu', 'lv', 'mg', 'mh', 'mi', 'mk', 'ml', 'mn', 'mr',
+    'ms', 'mt', 'my', 'na', 'nb', 'nd', 'ne', 'ng', 'nl', 'nn', 'no', 'nr',
+    'nv', 'ny', 'oc', 'oj', 'om', 'or', 'os', 'pa', 'pi', 'pl', 'ps', 'pt',
+    'qu', 'rm', 'rn', 'ro', 'ru', 'rw', 'sa', 'sc', 'sco', 'sd', 'se', 'sg',
+    'si', 'sk', 'sl', 'sm', 'sn', 'so', 'sq', 'sr', 'ss', 'st', 'su', 'sv',
+    'sw', 'ta', 'te', 'tg', 'th', 'ti', 'tk', 'tl', 'tn', 'to', 'tr', 'ts',
+    'tt', 'tw', 'ty', 'ug', 'uk', 'ur', 'uz', 've', 'vi', 'vo', 'wa', 'wo',
+    'xh', 'yi', 'yo', 'za', 'zh', 'zu'
 ]
 
 def _setup():
@@ -74,5 +74,5 @@ def _setup():
 _setup()
 if __name__ == '__main__':
     for code in SUPPORTED_LANGUAGE_CODES:
-        print("%4s %s" % (code, CODE_TO_ENGLISH_NAME[code]))
+        print("%-4s %s" % (code, CODE_TO_ENGLISH_NAME[code]))
 
---
+++ data/codes/README.txt
@@ -0,0 +1,12 @@
+`iso639.txt` is an incomplete mapping between ISO language codes and English
+language names.
+
+It was retrieved from
+http://download.geonames.org/export/dump/iso-languagecodes.txt, a file that is
+part of the GeoNames geographical database.  GeoNames is released under a
+Creative Commons Attribution 3.0 license, and is hosted at http://geonames.org.
+
+Some parenthetical clarifications were removed: for example, we've removed "
+(macrolanguage)" from the ends of a few language names, renamed "Modern Greek
+(1453-)" to "Greek", and removed date ranges from language names that were
+already unique.
