c7c81cdf7b8648f7f5347a7940c846b3042a098e
Rob Speer <rob@luminoso.com>
2014-02-24 16:59:17 -0500

    Update, clean up, and comment conceptnet5.builders.combine_assertions

--- conceptnet5/builders/combine_assertions.py
+++ conceptnet5/builders/combine_assertions.py
@@ -1,42 +1,70 @@
+from __future__ import unicode_literals, print_function
 import codecs
-from conceptnet5.edges import make_edge, MultiWriter
-from conceptnet5.nodes import make_disjunction_uri
-from collections import defaultdict
-from multiprocessing import Process
-import time
+from conceptnet5.edges import make_edge
+from conceptnet5.nodes import disjunction_uri
 import os
-import sys
 import json
 import math
 
 N = 100
 CURRENT_DIR = os.getcwd()
 
+
+def weight_scale(weight):
+    """
+    Put the weight of an assertion on a log_2 scale.
+    """
+    return math.log(max(1, weight + 1)) / math.log(2)
+
+
 def combine_assertions(csv_filename, out_filename, dataset, license):
+    """
+    Take in a tab-separated, sorted "CSV" file, named `csv_filename`, of
+    distinct edges which should be grouped together into assertions. Output a
+    JSON stream of assertions to `out_filename`.
+
+    The combined assertions will all have the same dataset and license,
+    unlike the edges that comprise them. These are specified as the `dataset`
+    and `license` arguments.
+
+    This process requires its input to be a sorted CSV so that all edges for
+    the same assertion will appear consecutively.
+    """
+    # The current_... variables accumulate information about the current
+    # assertion. When the URI changes, we can output this assertion.
     current_uri = None
     current_data = {}
     current_surface = None
     current_weight = 0.
     current_sources = []
+
     out = codecs.open(out_filename, 'w', encoding='utf-8')
     for line in codecs.open(csv_filename, encoding='utf-8'):
+        # Interpret the columns of the file.
         uri, rel, start, end, context, weight, source_uri, id, this_dataset, surface = line.split('\t')[:10]
         weight = float(weight)
         surface = surface.strip()
+
+        # If the uri is 'uri', this was a header line, so ignore it.
         if uri == 'uri':
             continue
+
+        # If the uri is the same as current_uri, accumulate more information.
         if uri == current_uri:
             current_weight += weight
             current_sources.append(source_uri)
+            # We use the first surface form we see as the surface form for the whole assertion.
             if (current_surface is None) and surface:
                 current_surface = surface
+
+        # Otherwise, it's a new assertion.
         else:
             if current_uri is not None:
                 output_assertion(out,
                     dataset=dataset, license=license,
                     sources=current_sources,
                     surfaceText=current_surface,
-                    weight=current_weight,
+                    weight=weight_scale(current_weight),
                     uri=current_uri,
                     **current_data
                 )
@@ -49,30 +77,45 @@ def combine_assertions(csv_filename, out_filename, dataset, license):
             current_weight = weight
             current_sources = [source_uri]
             current_surface = surface or None
-    
+
     output_assertion(out,
         rel=rel, start=start, end=end,
         dataset=dataset, license=license,
         sources=current_sources,
         surfaceText=current_surface,
-        weight=current_weight,
+        weight=weight_scale(current_weight),
         uri=current_uri
     )
     out.close()
 
+
 def output_assertion(out, **kwargs):
+    """
+    Output an assertion to the given output stream. All keyword arguments
+    become arguments to `make_edge`. (An assertion is a kind of edge.)
+    """
+    # Remove the URI, because make_edge computes it for us.
     uri = kwargs.pop('uri')
-    source_tree = make_disjunction_uri(set(kwargs.pop('sources')))
+
+    # Combine the sources into one AND-OR tree.
+    sources = set(kwargs.pop('sources'))
+    source_tree = disjunction_uri(*sources)
+
+    # Build the assertion object.
     assertion = make_edge(sources=source_tree, **kwargs)
-    current_weight = assertion['weight']
-    log_weight = math.log(max(1, current_weight + 1)) / math.log(2)
-    assertion['weight'] = log_weight
     
+    # Make sure the computed URI is the same as the one we had.
     assert assertion['uri'] == uri, (assertion['uri'], uri)
+
+    # Output the result in a JSON stream.
     line = json.dumps(assertion, ensure_ascii=False)
-    print >> out, line
+    print(line, out=out)
+
 
 if __name__ == '__main__':
+    # This is the main command-line entry point, used in steps of building
+    # ConceptNet that need to combine edges into assertions. See data/Makefile
+    # for more context.
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('input', help='csv file of input')
