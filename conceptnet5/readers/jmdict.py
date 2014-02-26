8c6df7ceb43489ebd7e91941c7d65d6093f2ed07
Rob Speer <rob@luminoso.com>
2014-02-26 13:56:16 -0500

    update JMdict reader

--- conceptnet5/readers/jmdict.py
+++ conceptnet5/readers/jmdict.py
@@ -1,10 +1,11 @@
+from __future__ import unicode_literals, print_function
 import xmltodict
-import sys
 import re
 import json
 import codecs
-import pycountry
-from conceptnet5.nodes import make_concept_uri
+from conceptnet5.util.language_codes import CODE_TO_ENGLISH_NAME, ENGLISH_NAME_TO_CODE
+from conceptnet5.json_stream import JSONStreamWriter
+from conceptnet5.uri import concept_uri, Licenses
 from conceptnet5.edges import make_edge
 
 # I took the time to record these, but in the end I don't think I plan
@@ -43,13 +44,19 @@ VERB_TYPES = [
     "suru verb ",
 ]
 
+
 def convert_lang_code(code):
-    language = pycountry.languages.get(bibliographic=code)
-    try:
-        short_code = language.alpha2
-    except AttributeError:
-        short_code = language.terminology
-    return short_code
+    """
+    Map a language code to the canonical one that ConceptNet 5 uses,
+    which is either the alpha2 code, or the "terminology" alpha3 code if no
+    alpha2 code exists
+    
+    JMdict uses the "bibliographic" alpha3 code, which is often equal to
+    neither of these, but we can map it to a canonical one by running it back
+    and forth through our language name dictionaries.
+    """
+    return ENGLISH_NAME_TO_CODE[CODE_TO_ENGLISH_NAME[code]]
+
 
 def get_list(node, tag):
     """
@@ -62,6 +69,7 @@ def get_list(node, tag):
     else:
         return [subnode]
 
+
 def get_one(node, tag):
     """
     Get one sub-node of this node by its tag. If there are none, return None.
@@ -71,6 +79,7 @@ def get_one(node, tag):
         return subnode[0]
     else:
         return subnode
+
 
 GLOSS_RE = re.compile(r'''
     # Separate out text in parentheses or brackets.
@@ -87,10 +96,12 @@ def parse_gloss(text):
     else:
         return None
 
+
+# TODO: write comments and a docstring.
 def read_jmdict(filename, outfilename):
-    file = open(filename)
-    outfile = codecs.open(outfilename, 'w', encoding='utf-8')
-    data = file.read().decode('utf-8')
+    file = codecs.open(filename, encoding='utf-8')
+    out = JSONStreamWriter(outfilename)
+    data = file.read()
     file.close()
 
     xml = xmltodict.parse(data)
@@ -101,7 +112,6 @@ def read_jmdict(filename, outfilename):
             headwords = [word['reb'] for word in get_list(entry, 'r_ele')]
         
         for sense in get_list(entry, 'sense'):
-            pos = get_one(sense, 'pos')
             glosses = get_list(sense, 'gloss') + get_list(sense, 'lsource')
             for gloss in glosses:
                 if '#text' in gloss:
@@ -109,21 +119,34 @@ def read_jmdict(filename, outfilename):
                     if '.' not in text:
                         lang = convert_lang_code(gloss['@xml:lang'])
                         for head in headwords:
-                            ja_concept = make_concept_uri(head, 'ja')
-                            other_concept = make_concept_uri(text, lang)
+                            ja_concept = concept_uri('ja', head)
+                            other_concept = concept_uri(lang, text)
                             if len(other_concept.split('_')) <= 5:
-                                output_edge(outfile, ja_concept, other_concept)
-    outfile.close()
+                                output_edge(out, ja_concept, other_concept)
+    out.close()
 
-def output_edge(outfile, subj_concept, obj_concept):
+
+def output_edge(out, subj_concept, obj_concept):
+    """
+    Write an edge to `out`, an instance of JSONFileWriter.
+    """
     rel = '/r/TranslationOf'
     edge = make_edge(rel, subj_concept, obj_concept,
                      dataset='/d/jmdict',
-                     license='/l/CC/By-SA',
+                     license=Licenses.cc_sharealike,
                      sources=['/s/jmdict/1.07'],
-                     context='/ctx/all',
                      weight=0.5)
-    print >> outfile, json.dumps(edge, ensure_ascii=False)
+    out.write(edge)
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='XML copy of JMDict to read')
+    parser.add_argument('output', help='JSON-stream file to output to')
+    args = parser.parse_args()
+    read_jmdict(args.input, args.output)
+
 
 if __name__ == '__main__':
-    read_jmdict(sys.argv[1], sys.argv[2])
+    main()
