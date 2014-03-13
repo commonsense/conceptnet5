1c44bcc86d6af30571f582cc80b0d7a4707d1b00
Rob Speer <rob@luminoso.com>
2014-03-13 18:20:54 -0400

    add Flask as a dependency

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author = "Rob Speer, Commonsense Computing Group",
     author_email = 'conceptnet@media.mit.edu',
     packages=packages,
-    install_requires=['metanl', 'assoc-space', 'xmltodict', 'pyyaml'],
+    install_requires=['metanl', 'assoc-space', 'xmltodict', 'pyyaml', 'flask'],
     license = 'GPLv3'
 )
 
