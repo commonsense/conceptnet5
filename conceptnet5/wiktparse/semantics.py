b16dbb3b263a5f9b3b689bccafabe1c160fe806f
Rob Speer <rob@luminoso.com>
2014-04-30 19:57:13 -0400

    Added some fancy template parsing, but keyword args aren't working

--- conceptnet5/wiktparse/semantics.py
+++ conceptnet5/wiktparse/semantics.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from conceptnet5.wiktparse.parser import wiktionaryParser, wiktionarySemantics
 from pprint import pprint
-
+from collections import defaultdict
 
 string_type = type('')
 
@@ -31,6 +31,9 @@ class EdgeInfo(object):
         self.pos = pos
         self.sense = sense
         self.rel = rel
+
+    def set_language(self, language):
+        return EdgeInfo(language, self.target, self.pos, self.sense, self.rel)
 
     def set_pos(self, pos):
         return EdgeInfo(self.language, self.target, pos, self.sense, self.rel)
@@ -101,8 +104,21 @@ class ConceptNetWiktionarySemantics(wiktionarySemantics):
             # We don't like off-Wiktionary links
             links = []
         else:
+            # Some entries specify their language using a hash-reference to
+            # that language's section of the page.
+            #
+            # TODO: Does anyone ever, for example, link to the French
+            # definition of the same surface word with a link that just says
+            # [[#French]]? What text even shows up in that case? How do we
+            # figure out what the name of the current entry is, so we can
+            # point to it?
+            language = self.default_language
+            target = ast['target']
+            if target.startswith('#'):
+                language = ast['target'][1:]
+                target = ast['text']
             links = [EdgeInfo(
-                language=self.default_language,
+                language=language,
                 target=ast['target']
             )]
         text = ast['text'] or ast['target']
@@ -112,10 +128,90 @@ class ConceptNetWiktionarySemantics(wiktionarySemantics):
         # Keep only the text of external links
         return LinkedText(text=ast['text'], links=[])
 
+    def etyl_link(self, ast):
+        language = ast['etyl']['language']
+        links = [link.set_language(language)
+                 for link in ast['link'].links]
+        return LinkedText(text=ast['link'].text, links=links)
+    
     def link_template(self, ast):
         # This is going to be complicated. We need to figure out the
         # argument structure of many different templates.
-        raise NotImplementedError
+        args = defaultdict(lambda: None)
+        links = []
+        
+        # Extract the text values of all arguments, and collect their links
+        # if they happen to have any
+        for key, value in ast['args'].items():
+            if value is not None:
+                args[key] = value.text
+                links.extend(value.links)
+
+        text = ''
+
+        linktype = ast['linktype']
+        if linktype == 'l' and ast['subtypes']:
+            language = ast['subtypes'][0]
+            target = args[1]
+            links = [EdgeInfo(language=language, target=target)]
+            text = target
+        
+        elif linktype in ('l', 'term/t'):
+            language = args[1]
+            target = args[2]
+            text = args[3] or target
+            links = [EdgeInfo(language=language, target=target)]
+        
+        elif linktype == 'term':
+            # {{term}} without a language really is in an unspecified language.
+            language = args['lang']
+            target = args[1]
+            text = args[2] or target
+            if text == 'byspel':
+                assert language is not None, ast
+            links = [EdgeInfo(language=language, target=target, rel='DerivedFrom')]
+
+        elif linktype == 'ja-l':
+            language = 'ja'
+            text = target = args[1]
+            links = [EdgeInfo(language=language, target=target)]
+        
+        elif linktype == 'ko-inline':
+            language = 'ko'
+            text = target = args[1]
+            links = [EdgeInfo(language=language, target=target)]
+        
+        # Cases below here don't need to set 'text', because they're only used
+        # in etymologies
+        elif linktype in ('back-form', 'clipping', '-er',):
+            language = args['lang'] or self.default_language
+            links = [EdgeInfo(language=language, target=args[1], rel='DerivedFrom')]
+        
+        elif linktype in ('borrowing'):
+            links = [EdgeInfo(language=args[1], target=args[2], rel='DerivedFrom')]
+        
+        elif linktype in ('blend', 'calque', 'compound', 'confix', 'prefix', 'suffix'):
+            # TODO: 'calque' has extra parameters, 'etyl lang' and 'etyl term',
+            # providing the link to the language being calqued from
+            language = args['lang'] or self.default_language
+            links = [
+                EdgeInfo(language=language, target=args[1], rel='DerivedFrom'),
+                EdgeInfo(language=language, target=args[2], rel='DerivedFrom')
+            ]
+            if args[3]:
+                links.append(EdgeInfo(language=language, target=args[3], rel='DerivedFrom'))
+
+        elif linktype == 'etycomp':
+            # Complex compound word etymologies
+            lang1 = args['lang1'] or self.default_language
+            lang2 = args['lang2'] or args['lang1'] or self.default_language
+            links = [
+                EdgeInfo(language=lang1, target=args[1], rel='DerivedFrom'),
+                EdgeInfo(language=lang2, target=args[2], rel='DerivedFrom')
+            ]
+
+        return LinkedText(text=text, links=links)
+
 
     def template_args(self, ast):
         """
