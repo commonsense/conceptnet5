61b1a8e2965ac950b90c8bb5bc5021bb561f536d
Rob Speer <rob@luminoso.com>
2015-06-08 13:55:09 -0400

    tweaks to assoc-space building

--- conceptnet5/wiktparse/de_parser.py
+++ conceptnet5/wiktparse/de_parser.py
@@ -15,7 +15,7 @@ from __future__ import print_function, division, absolute_import, unicode_litera
 from grako.parsing import graken, Parser
 
 
-__version__ = (2015, 4, 20, 15, 46, 49, 0)
+__version__ = (2015, 6, 3, 23, 34, 33, 2)
 
 __all__ = [
     'de_wiktionaryParser',
