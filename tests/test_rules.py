b0483a4d24e8a063cc811d5c86192823f23cf9b2
Joshua Chin <jchin@luminoso.com>
2015-07-08 16:27:27 -0400

    removed extra whitespace

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from nose.tools import eq_
 
 from conceptnet5.wiktparse.rules import (
-    EdgeInfo,  LinkedText,
+    EdgeInfo, LinkedText,
     EnWiktionarySemantics, DeWiktionarySemantics
 )
 
