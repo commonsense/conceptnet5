aa51fed0664873163ae942878be32c7f678b9fc9
Rob Speer <rob@luminoso.com>
2015-07-29 11:33:21 -0400

    fix some build problems

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=[
         'nltk >= 3.0b1', 'xmltodict', 'pyyaml', 'requests',
         'flask', 'flask-cors', 'flask-limiter', 'grako > 3', 'ftfy',
-        'msgpack-python'
+        'msgpack-python', 'langcodes >= 1.1.1'
     ],
     # assoc-space >= 1.0b1 is required for using assoc-space features, but it's
     # not required for all of ConceptNet
