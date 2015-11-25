8f76ba7e9d9462efd966ff75a19aee3f009f5dc9
Rob Speer <rob@luminoso.com>
2015-11-25 14:27:27 -0500

    filter out particular bad sources in the ConceptNet import

---
+++ conceptnet5/util/node_filters.py
@@ -0,0 +1,16 @@
+FILTERED_SOURCES = [
+    '/s/contributor/omcs/bntman',
+    '/s/contributor/omcs/cyberguy',
+    '/s/contributor/omcs/ddiblasi',
+    '/s/contributor/omcs/glneumiller',
+    '/s/contributor/omcs/imn8xtc',
+    '/s/contributor/omcs/holyrobot',
+    '/s/contributor/omcs/lbeckwith',
+    '/s/contributor/omcs/maliki',
+    '/s/contributor/omcs/sarcastro98',
+    '/s/contributor/omcs/wellner',
+    '/s/activity/omcs/20_questions',
+    '/s/activity/omcs/picture_description',
+    '/s/activity/omcs/response_to_diagram',
+    '/s/activity/omcs/pycommons_question',
+]
