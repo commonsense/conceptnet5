d506685647fb39ae8016bb302e3da4e32e6122f0
Rob Speer <rob@luminoso.com>
2016-01-29 18:09:43 -0500

    start implementing retrofitting

---
+++ conceptnet5/vectors/retrofit.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from scipy.sparse import coo_matrix
+from sklearn.preprocessing import normalize
+
+
+def run_retrofit(row_labels, dense_frame, sparse_csr, iterations=10, verbose=True):
+    retroframe = pd.DataFrame(index=row_labels, columns=dense_frame.columns)
+    retroframe.update(dense_frame)
+    # weight = 2 for known vectors, 1 for unknown vectors
+    weights = retroframe[0].isnull() + 1
+    weight_array = weights.values[:, np.newaxis]
+    orig_vecs = retroframe.fillna(0).values
+
+    # Delete the frame we built, we won't need its indices again until the end
+    del retroframe
+
+    vecs = orig_vecs
+    for iteration in range(iterations):
+        if verbose:
+            print('Iteration %s of %s' % (iteration+1, iterations))
+            vecs = sparse_csr.dot(vecs)
+
+            # use sklearn's normalize because it normalizes zero-rows to 0
+            normalize(vecs, norm='l2', copy=False)
+
+            # Average known rows with original vectors
+            vecs += orig_vecs
+            vecs /= weight_array
+
+    retroframe = pd.DataFrame(data=vecs, index=row_labels, columns=dense_frame.columns)
+    return retroframe
--- conceptnet5/vectors/sparse_matrix_builder.py
+++ conceptnet5/vectors/sparse_matrix_builder.py
@@ -1,6 +1,7 @@
 from scipy import sparse
 import pandas as pd
 from ordered_set import OrderedSet
+from .standardize import replace_numbers
 import re
 
 class SparseMatrixBuilder:
@@ -25,38 +26,20 @@ class SparseMatrixBuilder:
                                  shape=shape, dtype=dtype).tocsr()
 
 
-# TODO: move somewhere more appropriate
-DOUBLE_DIGIT_RE = re.compile(r'[0-9][0-9]')
-DIGIT_RE = re.compile(r'[0-9]')
-
-
-def replace_numbers(s):
-    """
-    Replace digits with # in any term where a sequence of two digits appears.
-
-    This operation is applied to text that passes through word2vec, so we
-    should match it.
-    """
-    if DOUBLE_DIGIT_RE.search(s): 
-        return DIGIT_RE.sub('#', s)
-    else:
-        return s
-
-
-def build_from_conceptnet_table(filename):
+def build_from_conceptnet_table(filename, orig_index=()):
     """
     Read a file of tab-separated association data from ConceptNet, such as
     `data/assoc/reduced.csv`. Return a SciPy sparse matrix of the associations,
-    and an OrderedSet of labels.
+    and a pandas Index of labels.
     """
     mat = SparseMatrixBuilder()
 
     # TODO: rebalance by dataset? Or maybe do that when building the
     # associations in the first place.
     
-    labels = OrderedSet()
+    labels = OrderedSet(orig_index)
 
-    with open(filename, encoding='utf-8') as infile:
+    with open(str(filename), encoding='utf-8') as infile:
         for line in infile:
             concept1, concept2, value_str, dataset, relation = line.strip().split('\t')
 
@@ -67,26 +50,5 @@ def build_from_conceptnet_table(filename):
             mat[index2, index1] = value
 
     shape = (len(labels), len(labels))
-    return mat.to_csr(shape), labels
-
-
-def table_to_multi_index(filename):
-    """
-    Read a file of tab-separated association data from ConceptNet, such as
-    `data/assoc/reduced.csv`. Return the result as a Pandas data structure:
-    in particular, a multi-indexed Series.
-    """
-    index_tuples = []
-    values = []
-    with open(str(filename), encoding='utf-8') as infile:
-        for line in infile:
-            concept1, concept2, value_str, dataset, relation = line.strip().split('\t')
-            if not concept1.endswith('/neg') and not concept2.endswith('/neg') and concept1 != concept2:
-                concept1 = replace_numbers(concept1)
-                concept2 = replace_numbers(concept2)
-                index_tuples.append((concept1, concept2))
-                values.append(float(value_str))
-
-    index = pd.MultiIndex.from_tuples(index_tuples, names=['start', 'end'])
-    series = pd.Series(values, index=index)
-    return series.sort_index()
+    index = pd.Index(labels)
+    return mat.tocsr(shape), index
