039c890ac8a91d8dc51d77e898c41f8809cbcf49
Rob Speer <rob@luminoso.com>
2016-06-23 18:24:16 -0400

    inculde reference data to test against

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@ export CONCEPTNET_BUILD_TEST=1
 build_test () {
     snakemake -q clean
     snakemake -j 4 test
-    diff -ur -x '*.msgpack' testdata/edges testdata/reference/edges
-    diff -ur -x '*.msgpack' testdata/assertions testdata/reference/assertions
-    diff -ur -x '*.msgpack' testdata/external testdata/reference/external
-    diff -ur -x '*.msgpack' testdata/assoc testdata/reference/assoc
+    diff -ur -x '*.msgpack' testdata/reference/edges testdata/edges
+    diff -ur -x '*.msgpack' testdata/reference/assertions testdata/assertions
+    diff -ur -x '*.msgpack' testdata/reference/external testdata/external
+    diff -ur -x '*.msgpack' testdata/reference/assoc testdata/assoc
 }
 
 nosetests --with-doctest conceptnet5 && build_test && nosetests && echo "Success."
