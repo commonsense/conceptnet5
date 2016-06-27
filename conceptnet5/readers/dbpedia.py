1548f89e4114588c30b6322d20157a0b986ba9c6
Rob Speer <rob@luminoso.com>
2016-06-27 11:38:12 -0400

    WIP on DBPedia

--- conceptnet5/readers/dbpedia.py
+++ conceptnet5/readers/dbpedia.py
@@ -1,32 +1,22 @@
 """
 Get data from DBPedia.
 """
-
 from conceptnet5.language.token_utils import un_camel_case
 from conceptnet5.uri import Licenses
-from conceptnet5.nodes import standardized_concept_uri
+from conceptnet5.nodes import (
+    standardized_concept_uri, standardize_topic, ALL_LANGUAGES, LCODE_ALIASES
+)
 from conceptnet5.edges import make_edge
 from conceptnet5.formats.msgpack_stream import MsgpackStreamWriter
-from conceptnet5.formats.semantic_web import NTriplesWriter, NTriplesReader, resource_name
-import langcodes
+from conceptnet5.formats.semantic_web import (
+    resource_name, parse_nquads, ExternalReferenceWriter
+)
 import urllib
-import sys
-import re
-
-__author__ = 'Justin Venezuela (jven@mit.edu), Rob Speer (rspeer@mit.edu)'
-
-
-
-# Python 2/3 compatibility
-if sys.version_info.major >= 3:
-    quote = urllib.parse.quote
-    urlsplit = urllib.parse.urlsplit
-    parse_url = urllib.parse.urlparse
-else:
-    import urlparse
-    parse_url = urlparse.urlparse
-    urlsplit = urlparse.urlsplit
-    quote = urllib.quote
+import bz2
+import pathlib
+from operator import itemgetter
+import itertools
+parse_url = urllib.parse.urlparse
 
 
 RELATIONS = {
@@ -51,61 +41,31 @@ RELATIONS = {
     'notableIdea': '/r/dbpedia/notableIdea',
     'notableWork': '/r/dbpedia/notableWork',
     'occupation': '/r/dbpedia/occupation',
-
-    # Things to add later if they would help:
-    #   leader / movement / predecessor / successor
-    #   author
-    #   writer / director / producer / starring / etc.
-    #   associatedBand / associatedMusicalArtist / bandMember
-    #   kingdom / phylum / ...
-
+    'author': '/r/dbpedia/author',
+    'writer': '/r/dbpedia/writer',
+    'director': '/r/dbpedia/director',
+    'producer': '/r/dbpedia/producer',
+    'starring': '/r/dbpedia/starring',
+    'genus': '/r/dbpedia/genus',
+    'leader': '/r/dbpedia/leader',
+    'associatedBand': '/r/dbpedia/associatedBand',
+    'associatedMusicalArtist': '/r/dbpedia/associatedMusicalArtist',
+    'bandMember': '/r/dbpedia/bandMember',
+    'artist': '/r/dbpedia/artist',
+    'musicalArtist': '/r/dbpedia/artist',
+    'musicalBand': '/r/dbpedia/artist'
 }
 
 # Ban some concepts that are way too generic and often differ from the common
 # way that people use these words
 CONCEPT_BLACKLIST = {
-    '/c/en/work', '/c/en/agent', '/c/en/artist',
-    '/c/en/thing', '/c/en/settlement'
+    '/c/en/work/n', '/c/en/agent/n', '/c/en/artist/n', '/c/en/thing/n'
 }
 
-# Every item has one type in 'instancetypes'. There are many types that have
-# lots of objects we're not interested in representing in ConceptNet, such as
-# 'Road', 'RailwayStation', and 'Album'. Here are the types we *are* okay with.
-TYPE_WHITELIST = {
-    'Insect',
-    'Thing',
-    'Plant',
-    'City',
-    'Fish',
-    'Mammal',
-    'Country',
-    'AnatomicalStructure',
-    'Language',
-    'Animal',
-    'Drug',
-    'Weapon',
-    'Disease',
-    'Reptile',
-    'Food',
-    'Planet',
-    'MountainRange',
-    'Game',
-    'Mineral',
-    'MusicGenre',
-    'ProgrammingLanguage',
-    'Colour',
-    'Holiday',
-    'Eukaryote',
-    'WorldHeritageSite',
-    'Fern',
-    'Conifer',
-    'Fashion',
-    'Bone',
-    'Currency',
-    'Nerve',
-    'Muscle',
-    'Sport',
-    'Continent',
+TYPE_BLACKLIST = {
+    'Settlement', 'Railway Line', 'Road', 'Sports Event',
+    'Olympic Event', 'Soccer Tournament', 'Election', 'Diocese',
+    'Year', 'Football League Season',
 }
 
 
@@ -126,29 +86,31 @@ def translate_dbpedia_url(url):
     The URL itself is a stable thing that we can build a ConceptNet URI from,
     on the other hand.
     """
+    if '__' in url:
+        return None
     parsed = parse_url(url)
     domain = parsed.netloc
 
     if domain == 'dbpedia.org':
         # Handle old DBPedia URLs that had no language code
-        lang = 'en'
-    else:
-        domain_parts = domain.split('.', 1)
-        if domain_parts[1] == 'dbpedia.org':
-            lang = domain_parts[0]
+        domain = 'en.dbpedia.org'
 
-            # If we can't name this language in English, it's probably
-            # not really a language.
-            if langcodes.get(lang).language_name('en') == lang:
-                return None
-        else:
+    domain_parts = domain.split('.', 1)
+    if domain_parts[1] == 'dbpedia.org':
+        lang = domain_parts[0]
+        if lang in LCODE_ALIASES:
+            lang = LCODE_ALIASES[lang]
+        if lang not in ALL_LANGUAGES:
             return None
+        text = resource_name(url).replace('_', ' ')
+        uri = standardize_topic(lang, text)
+        if uri in CONCEPT_BLACKLIST:
+            return None
+        else:
+            return uri
+    else:
+        return None
 
-    # Some Semantic Web URLs are camel-cased. ConceptNet URIs use underscores
-    # between words.
-    pieces = parse_topic_name(resource_name(url))
-    pieces[0] = un_camel_case(pieces[0])
-    return standardized_concept_uri(lang, *pieces)
 
 def map_dbpedia_relation(url):
     """
@@ -170,90 +132,84 @@ def map_dbpedia_relation(url):
         return None
 
 
-def handle_file(filename, output_file, sw_map_file):
-    reader = NTriplesReader()
+def process_dbpedia(input_dir, output_file, refs_file):
+    input_path = pathlib.Path(input_dir)
+    interlang_path = input_path / 'interlanguage_links_en.tql.bz2'
+    mapped_urls = interlanguage_mapping(interlang_path)
+
+    ok_concepts = set()
     out = MsgpackStreamWriter(output_file)
-    map_out = NTriplesWriter(sw_map_file)
-    for line in open(filename, 'rb'):
-        if not line.startswith(b'#'):
-            handle_triple(line.decode('utf-8').strip(), reader, out, map_out)
+    refs = ExternalReferenceWriter(refs_file)
+
+    types_path = input_path / 'instance_types_en.tql.bz2'
+    quads = parse_nquads(bz2.open(str(types_path), 'rt'))
+    for subj, pred, obj, _graph in quads:
+        subj_url = subj['url']
+        if (
+            'Category:' in subj_url or 'File:' in subj_url or
+            'List_of' in subj_url or '__' in subj_url
+        ):
+            continue
+        if subj_url in mapped_urls:
+            subj_concept = translate_dbpedia_url(subj_url)
+            if subj_concept:
+                obj_type = un_camel_case(resource_name(obj['url']))
+                if obj_type not in TYPE_BLACKLIST:
+                    ok_concepts.add(subj_concept)
+                    obj_concept = standardized_concept_uri('en', obj_type, 'n')
+                    if obj_concept not in CONCEPT_BLACKLIST:
+                        ok_concepts.add(obj_concept)
+                        edge = make_edge(
+                            '/r/IsA', subj_concept, obj_concept,
+                            dataset='/d/dbpedia/en',
+                            license=Licenses.cc_sharealike,
+                            sources=[{'contributor': '/s/resource/dbpedia/2015/en'}],
+                            weight=0.5
+                        )
+                        out.write(edge)
+                    for other_url in mapped_urls[subj_url]:
+                        if other_url.startswith('http://wikidata.dbpedia.org/'):
+                            refs.write_link(subj_concept, other_url)
+                        else:
+                            other_concept = translate_dbpedia_url(other_url)
+                            if other_concept:
+                                refs.write_link(other_concept, other_url)
+                                edge = make_edge(
+                                    '/r/TranslationOf',
+                                    other_concept, subj_concept,
+                                    dataset='/d/dbpedia/en',
+                                    license=Licenses.cc_sharealike,
+                                    sources=[{'contributor': '/s/resource/dbpedia/2015/en'}],
+                                    weight=0.5
+                                )
+                                out.write(edge)
+    refs.close()
+    out.close()
 
 
-def handle_triple(line, reader, out, map_out):
-    subj, pred, obj, tag = reader.parse_line(line)
-    if tag != 'URL':
-        return
+def interlanguage_mapping(interlang_path):
+    quads = parse_nquads(bz2.open(str(interlang_path), 'rt'))
+    mapping = {}
+    for subj, values in itertools.groupby(quads, itemgetter(0)):
+        subj_url = subj['url']
+        vals_list = list(values)
 
-    # Ignore types of edges that we don't care about:
-    #   - Homepage links
-    #   - GIS features
-    #   - Assertions that something "is a thing"
-    #   - Anonymous nodes identified with double-underscores, such as the node
-    #     "Alfred_Nobel__1", which means "Alfred Nobel's occupation, whatever
-    #     it is"
-    #   - Nodes that are articles named "List of X" on Wikipedia
-    if (
-        'foaf/0.1/homepage' in pred or '_Feature' in obj or '#Thing' in obj or
-        '__' in subj or '__' in obj or 'List_of' in subj or 'List_of' in obj
-        or 'Wikidata:' in obj
-    ):
-        return
+        # Keep nodes with at least 5 translations plus the Wikidata links
+        if len(vals_list) >= 7:
+            concepts = [subj_url]
+            for _subj, _pred, obj, _graph in vals_list:
+                url = obj['url']
+                if 'www.wikidata.org' in url:
+                    continue
+                if url.startswith('http://wikidata.dbpedia.org/'):
+                    wikidata_id = resource_name(url)
 
-    # We don't try to parse URIs from outside of dbpedia.org's namespace.
-    if 'dbpedia.org' not in obj:
-        return
+                    # Return early when we see a high-numbered Wikidata ID
+                    print(wikidata_id, resource_name(subj_url))
+                    if int(wikidata_id[1:]) >= 50000:
+                        return mapping
+                concepts.append(url)
 
-    subj_concept = translate_dbpedia_url(subj)
-    obj_concept = translate_dbpedia_url(obj)
-    subj_text = un_camel_case(parse_topic_name(resource_name(subj))[0])
-    obj_text = un_camel_case(parse_topic_name(resource_name(obj))[0])
-    if subj_concept is None or obj_concept is None:
-        return
-
-    # DBPedia categorizes a lot of things as 'works', which causes unnecessary
-    # ambiguity. Disregard these edges; there will almost always be a more
-    # specific edge calling it a 'creative work' anyway.
-    if obj_concept in CONCEPT_BLACKLIST:
-        return
-
-    rel = map_dbpedia_relation(pred)
-    if rel is None:
-        return
-
-    if rel in {'/r/IsA', '/r/TranslationOf'}:
-        obj_text = obj_text.lower()
-
-    # We've successfully converted this Semantic Web triple to ConceptNet URIs.
-    # Now write the results to the 'sw_map' file so others can follow this
-    # mapping.
-    mapped_pairs = [
-        (pred, rel),
-        (subj, subj_concept),
-        (obj, obj_concept)
-    ]
-    for sw_url, conceptnet_uri in mapped_pairs:
-        conceptnet_url = full_conceptnet_url(conceptnet_uri)
-        map_out.write_link(conceptnet_url, sw_url)
-
-    edge = make_edge(rel, subj_concept, obj_concept,
-                     dataset='/d/dbpedia/en',
-                     license=Licenses.cc_sharealike,
-                     sources=['/s/dbpedia/2014'],
-                     surfaceText=make_surface_text(rel, subj_text, obj_text),
-                     weight=0.5)
-
-    out.write(edge)
+            mapping[subj_url] = concepts
 
 
-def main():
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input', help='N-triples file of input')
-    parser.add_argument('output', help='msgpack file to output to')
-    parser.add_argument('sw_map', help='An N-triples file of Semantic Web equivalences')
-    args = parser.parse_args()
-    handle_file(args.input, args.output, args.sw_map)
-
-
-if __name__ == '__main__':
-    main()
