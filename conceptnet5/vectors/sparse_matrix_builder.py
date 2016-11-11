6935835b83b6116d567babd26b7ac1ce8feb9f1b
Rob Speer <rob@luminoso.com>
2016-11-11 17:40:33 -0500

    WIP: feature table

--- conceptnet5/vectors/sparse_matrix_builder.py
+++ conceptnet5/vectors/sparse_matrix_builder.py
@@ -1,6 +1,7 @@
 from scipy import sparse
 import pandas as pd
 from conceptnet5.uri import uri_prefixes
+from conceptnet5.relations import SYMMETRIC_RELATIONS
 from ordered_set import OrderedSet
 from collections import defaultdict
 from ..vectors import replace_numbers
@@ -47,6 +48,65 @@ def build_from_conceptnet_table(filename, orig_index=(), self_loops=True):
     with open(str(filename), encoding='utf-8') as infile:
         for line in infile:
             concept1, concept2, value_str, dataset, relation = line.strip().split('\t')
+
+            index1 = labels.add(replace_numbers(concept1))
+            index2 = labels.add(replace_numbers(concept2))
+            value = float(value_str)
+            mat[index1, index2] = value
+            mat[index2, index1] = value
+            totals[index1] += value
+            totals[index2] += value
+
+    # Link nodes to their more general versions
+    for label in labels:
+        prefixes = list(uri_prefixes(label, 3))
+        if len(prefixes) >= 2:
+            parent_uri = prefixes[-2]
+            if parent_uri in labels:
+                index1 = labels.index(label)
+                index2 = labels.index(parent_uri)
+                mat[index1, index2] = 1
+                mat[index2, index1] = 1
+                totals[index1] += 1
+                totals[index2] += 1
+
+    # add self-loops on the diagonal with equal weight to the rest of the row
+    if self_loops:
+        for key, value in totals.items():
+            mat[key, key] = value
+
+    shape = (len(labels), len(labels))
+    index = pd.Index(labels)
+    return mat.tocsr(shape), index
+
+
+def build_features_from_conceptnet_table(filename):
+    mat = SparseMatrixBuilder()
+
+    concept_labels = OrderedSet()
+    feature_labels = OrderedSet()
+
+    totals = defaultdict(float)
+    with open(str(filename), encoding='utf-8') as infile:
+        for line in infile:
+            concept1, concept2, value_str, dataset, relation = line.strip().split('\t')
+            concept1 = replace_numbers(concept1)
+            concept2 = replace_numbers(concept2)
+            value = float(value_str)
+            if relation in SYMMETRIC_RELATIONS:
+                feature_pairs = [
+                    ('{} {} ~'.format(concept1, relation), concept2),
+                    ('{} {} ~'.format(concept2, relation), concept1)
+                ]
+            else:
+                feature_pairs = [
+                    ('{} {} -'.format(concept1, relation), concept2),
+                    ('- {} {}'.format(relation, concept2), concept1)
+                ]
+            for feature, concept in feature_pairs:
+                concept_index = concept_labels.add(concept)
+                feature_index = feature_labels.add(feature)
+                mat[concept_index, feature_index] = value
 
             index1 = labels.add(replace_numbers(concept1))
             index2 = labels.add(replace_numbers(concept2))
