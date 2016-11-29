816606b295317e3c8dc34637210e5d173b58053c
Rob Speer <rob@luminoso.com>
2016-11-29 16:08:51 -0500

    put the analogy print statements in context
    ...with another silly print statement

--- conceptnet5/vectors/evaluation/analogy.py
+++ conceptnet5/vectors/evaluation/analogy.py
@@ -106,6 +106,7 @@ def tune_pairwise_analogies(frame, eval_filename, subset):
     #     0.25, 0.3, 0.4, 0.5, 0.6, 0.8,
     #     1.0, 1.2, 1.5, 2.0, 2.5, 3.0, 4.0
     # ]
+    print('Tuning analogy weights')
     weights = [
         0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55,
         0.6, 0.65, 0.7, 0.75, 0.8, 0.9, 1.0
