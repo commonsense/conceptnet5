3d860a9860d8476ec913f81db74a19ea78c19175
Rob Speer <rob@luminoso.com>
2017-05-08 15:10:08 -0400

    WIP: tried drawing constellation-like edges

--- viz/conceptnet_viz/svgmap.py
+++ viz/conceptnet_viz/svgmap.py
@@ -1,5 +1,6 @@
 import svgwrite
 import networkx as nx
+import numpy as np
 from networkx.exception import NetworkXError
 from networkx.algorithms.mst import minimum_spanning_tree
 from scipy.spatial import Delaunay
@@ -46,20 +47,28 @@ def spanning_tree(frame):
 
     print("Getting spanning tree")
     spantree = minimum_spanning_tree(graph)
+    newgraph = nx.Graph(graph)
 
-    for i in range(len(coords)):
-        for j in range(neighbor_indices[i], neighbor_indices[i+1]):
-            neighbor = neighbor_indptr[j]
-            distance = euclidean(coords[i], coords[neighbor])
-            if distance < 0.1:
-                spantree.add_edge(i, neighbor, weight=distance)
+    for i in graph.nodes():
+        neighbors = [j for j in graph.neighbors(i) if not spantree.has_edge(i, j)]
+        if neighbors:
+            neighbors = np.array(neighbors)
+            distances = np.array(
+                [euclidean(coords[i], coords[j]) for j in neighbors]
+            )
+            distance_sort = np.argsort(distances)
+            worst_neighbors = neighbors[distance_sort[-2:]]
+            for worst in worst_neighbors:
+                if newgraph.has_edge(i, worst):
+                    newgraph.remove_edge(i, worst)
 
-    return spantree
+    return newgraph
 
 
 def draw_tsne(tsne_filename, degree_filename, out_filename, xmin=0, ymin=0, xmax=5000, ymax=5000):
     tsne_frame = load_hdf(tsne_filename)
-    spantree = spanning_tree(tsne_frame)
+    # spantree = spanning_tree(tsne_frame)
+    spantree = None
 
     concept_degrees = get_concept_degrees(degree_filename)
     draw = svgwrite.Drawing(size=(xmax - xmin, ymax - ymin))
@@ -77,32 +86,33 @@ def draw_tsne(tsne_filename, degree_filename, out_filename, xmin=0, ymin=0, xmax
         if xmin <= px <= xmax and ymin <= py <= ymax:
             px -= xmin
             py -= ymin
-            pt_size = deg ** .25 / 4
+            pt_size = deg ** .25 / 2
             circle = draw.circle(
                 center=(px, py), r=pt_size, opacity=0.75,
                 style="fill: %s;" % fill_color
             )
             draw.add(circle)
 
-            try:
-                neighbors = spantree.neighbors(i)
-                for neighbor_idx in neighbors:
-                    if neighbor_idx > i:
-                        nx = tsne_frame.iloc[neighbor_idx, 0]
-                        ny = tsne_frame.iloc[neighbor_idx, 1]
-                        pnx = tsne_to_svg_coordinate(nx) - xmin
-                        pny = tsne_to_svg_coordinate(ny) - ymin
-                        ndeg = concept_degrees.get(tsne_frame.index[neighbor_idx], 0)
-                        weight = (deg * ndeg) ** .125 / 10
-                        line = draw.line(
-                            (px, py),
-                            (pnx, pny),
-                            opacity=0.75,
-                            style="stroke: #016; stroke-width: %4.4f" % weight
-                        )
-                        draw.add(line)
-            except NetworkXError:
-                pass
+            if spantree is not None:
+                try:
+                    neighbors = spantree.neighbors(i)
+                    for neighbor_idx in neighbors:
+                        if neighbor_idx > i:
+                            nx = tsne_frame.iloc[neighbor_idx, 0]
+                            ny = tsne_frame.iloc[neighbor_idx, 1]
+                            pnx = tsne_to_svg_coordinate(nx) - xmin
+                            pny = tsne_to_svg_coordinate(ny) - ymin
+                            ndeg = concept_degrees.get(tsne_frame.index[neighbor_idx], 0)
+                            weight = (deg * ndeg) ** .125 / 10
+                            line = draw.line(
+                                (px, py),
+                                (pnx, pny),
+                                opacity=0.75,
+                                style="stroke: #348; stroke-width: %4.4f" % weight
+                            )
+                            draw.add(line)
+                except NetworkXError:
+                    pass
 
     for i, uri in enumerate(tsne_frame.index):
         deg = concept_degrees.get(uri, 0)
@@ -126,4 +136,4 @@ def draw_tsne(tsne_filename, degree_filename, out_filename, xmin=0, ymin=0, xmax
 
 if __name__ == '__main__':
     draw_tsne('viz/tsne-joined.h5', 'data/stats/concept_counts.txt', 'viz/svg/conceptnet-mid.svg',
-              xmin=2200, xmax=2800, ymin=2200, ymax=2800)
+              xmin=2000, xmax=3000, ymin=2000, ymax=3000)
