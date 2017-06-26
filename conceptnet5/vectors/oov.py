66b7839f7a84f7ad5f8395b77887dd5b71e5e251
jlowryduda <joanna.teresa.duda@gmail.com>
2017-06-26 12:05:41 -0400

    rudimentary subword oov

--- conceptnet5/vectors/oov.py
+++ conceptnet5/vectors/oov.py
@@ -1,11 +1,15 @@
-from conceptnet5.vectors.formats import load_hdf
-from conceptnet5.nodes import uri_to_label
-from conceptnet5.uri import get_language
-from itertools import combinations
 from collections import defaultdict
+from itertools import combinations
+
 import numpy as np
 import pandas as pd
+from scipy.sparse import dok_matrix, csr_matrix
+from scipy.sparse.linalg import svds
 from wordfreq import word_frequency
+
+from conceptnet5.nodes import uri_to_label
+from conceptnet5.uri import get_language
+from conceptnet5.vectors.formats import load_hdf
 from conceptnet5.vectors import cosine_similarity
 
 
@@ -71,45 +75,56 @@ def fill_data(matrix, subwords, subword_to_index, word_to_index):
 
 
 def get_subword_vectors(frame_filename, vocab_len, low, high):
-    # Prep
     frame = load_hdf(frame_filename)
     smaller_frame = get_smaller_frame(frame, vocab_len)
     subwords = get_all_subwords(smaller_frame, low, high)
     subword_to_index, word_to_index = make_indices(subwords, smaller_frame)
-    matrix = np.zeros((len(smaller_frame.index), len(subwords)))
-    filled_matrix = fill_data(matrix, subwords, subword_to_index, word_to_index)
 
-    U, Σ, Vt = np.linalg.svd(filled_matrix, full_matrices=False)
-    uframe = U[:,:300]
-    sframe = Σ[:300]
-    vframe = Vt.T[:, :300]
-    step1 = vframe * sframe
-    pinv = step1.dot(uframe.T)
-    n = smaller_frame.values
-    final = pinv.dot(n)
-    final_df = pd.DataFrame(final, index=list(subwords))
+    # fill a matrix with subword information
+    matrix = dok_matrix((len(smaller_frame.index), len(subwords)), dtype=np.float32)
+    for subword in subwords:
+        words = subwords[subword]
+        subword_index = subword_to_index[subword]
+        for word in words:
+            word_index = word_to_index[word]
+            matrix[word_index, subword_index] = 1
 
-    similarities = []
-    for term in smaller_frame.index:
-        word_subwords = get_word_subwords(low, high, term)
-        vectors = []
-        for subword in word_subwords:
-            try:
-                vector = np.array(final_df.loc[subword])
-                vectors.append(vector)
-            except KeyError:
-                continue
-        final_vector = np.mean(vectors, axis=0)
-        similarity = cosine_similarity(final_vector, smaller_frame.loc[term])
-        similarities.append(similarity)
-    print(np.mean(similarities))
-    return final
+    # version with sparse matrices
+    u, s, vt = svds(matrix, k=300)
+    u_sp = csr_matrix(u.T)
+    v_sp = csr_matrix(vt.T)
+    s_sp = csr_matrix(s)
+    step1 = v_sp.multiply(s_sp)
+    step2 = step1.dot(u_sp)  # pseudoinverse
 
+    # version with regular matrices
+    # matrix = np.zeros((len(smaller_frame.index), len(subwords)))
+    # filled_matrix = fill_data(matrix, subwords, subword_to_index, word_to_index)
 
+    # U, Σ, Vt = np.linalg.svd(filled_matrix, full_matrices=False)
+    # uframe = U[:,:300]
+    # sframe = Σ[:300]
+    # vframe = Vt.T[:, :300]
+    # step1 = vframe * sframe
+    # pinv = step1.dot(uframe.T) #pseudoinverse
+    # n = smaller_frame.values
+    # final = pinv.dot(n)
+    # final_df = pd.DataFrame(final, index=list(subwords))
+    #
 
-
-
-
-
-
-
+    # Evaluation
+    # similarities = []
+    # for term in smaller_frame.index:
+    #     word_subwords = get_word_subwords(low, high, term)
+    #     vectors = []
+    #     for subword in word_subwords:
+    #         try:
+    #             vector = np.array(final_df.loc[subword])
+    #             vectors.append(vector)
+    #         except KeyError:
+    #             continue
+    #     final_vector = np.mean(vectors, axis=0)
+    #     similarity = cosine_similarity(final_vector, smaller_frame.loc[term])
+    #     similarities.append(similarity)
+    # print(np.mean(similarities))
+    return step2
