500362448b7d49f627533b2784f21edf09a7d83a
Rob Speer <rob@luminoso.com>
2017-11-30 17:51:22 -0500

    SME: normalize embeddings at each step

--- conceptnet5/vectors/semantic_matching.py
+++ conceptnet5/vectors/semantic_matching.py
@@ -14,6 +14,7 @@ from conceptnet5.uri import uri_prefix, assertion_uri
 from conceptnet5.util import get_data_filename
 from conceptnet5.vectors.formats import load_hdf
 from conceptnet5.vectors.transforms import l2_normalize_rows
+from sklearn.preprocessing import normalize
 
 RELATION_INDEX = pd.Index(COMMON_RELATIONS)
 N_RELS = len(RELATION_INDEX)
@@ -117,6 +118,10 @@ class SemanticMatchingModel(nn.Module):
         self.assoc_tensor.data[0] = self.identity_slice
         self.rel_vecs.weight.data[0, :] = 0
         self.rel_vecs.weight.data[0, 0] = 1
+
+    def normalize_embeddings(self):
+        normalize(self.rel_vecs.weight, copy=False)
+        normalize(self.term_vecs.weight, copy=False)
 
     def forward(self, rels, terms_L, terms_R):
         # Get relation vectors for the whole batch, with shape (b * i)
@@ -303,6 +308,7 @@ def run():
         loss.backward()
         optimizer.step()
         model.reset_synonym_relation()
+        model.normalize_embeddings()
 
         losses.append(loss.data[0])
         steps += 1
