ecabd622601c75b1007e7c2579437ba01ea81d0e
Robyn Speer <rspeer@luminoso.com>
2019-03-28 17:10:26 -0400

    Remove the 'pairs' key, which I didn't need
    It seemed like a good idea but it was just duplicating something that
    GIN can already do.

--- conceptnet5/db/prepare_data.py
+++ conceptnet5/db/prepare_data.py
@@ -29,17 +29,30 @@ def sanitize(text):
 
 
 def gin_indexable_edge(edge):
+    """
+    Convert an edge into a dictionary that can be matched with the JSONB @>
+    operator, which tests if one dictionary includes all the information in
+    another. This operator can be indexed by GIN.
+
+    We replace the 'start', 'end', 'rel', and 'dataset' URIs with lists
+    of their URI prefixes. We query those slots with a single-element list,
+    which will be a sub-list of the prefix list if it's a match.
+
+    As an example, a query for {'start': '/c/en'} will become the GIN
+    query {'start': ['/c/en']}, which will match indexed edges such as
+    {
+        'start': ['/c/en', '/c/en/dog'],
+        'end': ['/c/en', '/c/en/bark'],
+        'rel': ['/r/CapableOf'],
+        ...
+    }
+    """
     gin_edge = {}
     gin_edge['uri'] = edge['uri']
     gin_edge['start'] = uri_prefixes(edge['start'])
     gin_edge['end'] = uri_prefixes(edge['end'])
     gin_edge['rel'] = uri_prefixes(edge['rel'])
     gin_edge['dataset'] = uri_prefixes(edge['dataset'])
-    gin_edge['pairs'] = [
-        '%s %s' % tuple(sorted([prefix1, prefix2]))
-        for prefix1 in gin_edge['start']
-        for prefix2 in gin_edge['end']
-    ]
     flat_sources = set()
     for source in edge['sources']:
         for value in source.values():
