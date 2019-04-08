5caa31ecf50af402aa9f9f0cbfacedc94c491633
Robyn Speer <rspeer@luminoso.com>
2019-04-08 14:01:08 -0400

    add comment to test-quick

--- test-quick.sh
+++ test-quick.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# If you've just run ./test.sh and you want to run it again with the same data,
+# this command will run the tests that depend on the test data without
+# rebuilding it.
 
 export CONCEPTNET_BUILD_TEST=1
 export CONCEPTNET_BUILD_DATA=testdata
