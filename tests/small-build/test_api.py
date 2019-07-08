1fbb973011b2709ca34e3184615dbf8263d2dffa
Robyn Speer <rspeer@luminoso.com>
2019-07-08 16:33:01 -0400

    remove leftover print statements

--- tests/small-build/test_api.py
+++ tests/small-build/test_api.py
@@ -5,7 +5,6 @@ from nose.tools import eq_
 def test_related_query():
     # Test that we can look up related terms
     result = api.query_related('/c/en/test', limit=3)
-    print(result)
     eq_(len(result['related']), 3)
 
 
@@ -13,6 +12,5 @@ def test_related_query_malformed():
     # Test that we fulfill a query for related terms to a nonsense URI, and
     # there are simply no results
     result = api.query_related('/c/en,test', limit=3)
-    print(result)
     eq_(len(result['related']), 0)
 
