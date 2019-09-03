a067b940fd18e178da7a9f5a73ed696dc03151ef
Robyn Speer <rspeer@luminoso.com>
2019-09-03 12:56:13 -0400

    parametrize test_lemmatize

--- conceptnet5/tests/small_build/test_lemmatizer.py
+++ conceptnet5/tests/small_build/test_lemmatizer.py
@@ -1,10 +1,19 @@
+import pytest
+
 from conceptnet5.language.lemmatize import lemmatize
 from conceptnet5.tests.conftest import run_build
 
 
-def test_lemmatize(run_build):
-    assert lemmatize('en', 'eating') == ('eat', 'pres+ptcp')
-    assert lemmatize('en', 'carrots') == ('carrot', 'p')
-    assert lemmatize('en', 'is') == ('be', '3+s+pres')
-    assert lemmatize('en', 'good') == ('good', '')
-    assert lemmatize('es', 'tengo', 'v') == ('tener', '1+s+pres+ind')
+LEMMA_EXAMPLES = [
+    (('en', 'eating'), ('eat', 'pres+ptcp')),
+    (('en', 'carrots'), ('carrot', 'p')),
+    (('en', 'is'), ('be', '3+s+pres')),
+    (('en', 'good'), ('good', '')),
+    (('es', 'tengo', 'v'), ('tener', '1+s+pres+ind')),
+]
+
+
+@pytest.mark.parametrize('example', LEMMA_EXAMPLES)
+def test_lemmatize(run_build, example):
+    args, output = example
+    assert lemmatize(*args) == output
