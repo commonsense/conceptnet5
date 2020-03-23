32ff3a80195387baa8982a39db6cbe30e5e1e608
Robyn Speer <rspeer@luminoso.com>
2020-03-23 16:45:32 -0400

    add a few unreliable OMCS contributors to blocklist

--- conceptnet5/builders/cli.py
+++ conceptnet5/builders/cli.py
@@ -14,7 +14,7 @@ def cli():
 @click.argument('input', type=click.Path(readable=True, dir_okay=False))
 @click.argument('core', type=click.Path(readable=True, dir_okay=False))
 @click.argument('output', type=click.Path(writable=True, dir_okay=False))
-def run_combine(input, output):
+def run_combine(input, core, output):
     """
     Combine edges that have the same relation, start, and end, into
     higher-level assertions that add their weights and sources.
