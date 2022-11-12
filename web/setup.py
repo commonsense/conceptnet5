88f0418aecbb9632e6aac5f0ab252fc4e8b86b15
Marcelo Machado <mmachado@ibm.com>
2022-11-12 03:14:05 -0300

    Fixed the versions of lib required in the project

--- web/setup.py
+++ web/setup.py
@@ -23,7 +23,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'conceptnet >= %s' % version_str,
-        'limits == 2.0.3', 'flask == 2.0.2', 'flask-cors == 3.0.10', 'flask-limiter == 2.7.0',
+        'limits == 2.3', 'flask == 2.0.2', 'flask-cors == 3.0.10', 'flask-limiter == 2.7.0',
         'langcodes == 3.3.0', 'jinja2-highlight == 0.6.1', 'pygments == 2.10.0', 'raven[flask] == 6.10.0'
     ],
     license = 'Apache License 2.0',
